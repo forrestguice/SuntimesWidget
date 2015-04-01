082c98b4839c825eb979b3a8ba169647f07dd261
Forrest Guice <forrestguice@gmail.com>
2015-04-01 07:41:46 -0700

    copyright notice
    added copyright notices

--- app/src/main/java/com/forrestguice/suntimeswidget/settings/SuntimesWidgetThemes.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/settings/SuntimesWidgetThemes.java
@@ -1,3 +1,22 @@
+/**
+   Copyright (C) 2014 Forrest Guice
+   This file is part of SuntimesWidget.
+
+   SuntimesWidget is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   SuntimesWidget is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package com.forrestguice.suntimeswidget.settings;
 
 import android.app.Application;
@@ -43,7 +62,7 @@ public class SuntimesWidgetThemes
     // Theme: Dark
     public static final String THEMEDEF_DARK_NAME = "dark";
     public static final String THEMEDEF_DARK_DISPLAYSTRING = "Dark";
-    public static final int THEMEDEF_DARK_BACKGROUND_ID = R.drawable.bg_dark;
+    public static final int THEMEDEF_DARK_BACKGROUND_ID = R.drawable.bg_widget_dark;
     public static final int THEMEDEF_DARK_TEXTCOLOR_ID = android.R.color.tertiary_text_dark;
     public static final int THEMEDEF_DARK_TITLECOLOR_ID = android.R.color.tertiary_text_dark;
     public static final int THEMEDEF_DARK_TIMESUFFIXCOLOR_ID = android.R.color.tertiary_text_dark;
