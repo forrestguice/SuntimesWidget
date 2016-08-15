a7ce5f3ddd37ee39c741a1e1df155e594bb39bb0
Forrest Guice <forrestguice@gmail.com>
2016-08-15 15:53:02 -0700

    add EquinoxSolsitceData

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/calculator/SuntimesEquinoxSolsticeData.java
@@ -0,0 +1,151 @@
+/**
+    Copyright (C) 2014 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+package com.forrestguice.suntimeswidget.calculator;
+
+import android.content.Context;
+
+import com.forrestguice.suntimeswidget.settings.WidgetSettings;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class SuntimesEquinoxSolsticeData extends SuntimesData
+{
+    private Context context;
+
+    public SuntimesEquinoxSolsticeData(Context context, int appWidgetId)
+    {
+        this.context = context;
+        initFromSettings(context, appWidgetId);
+    }
+    public SuntimesEquinoxSolsticeData(SuntimesEquinoxSolsticeData other)
+    {
+        this.context = other.context;
+        initFromOther(other);
+    }
+
+    /**
+     * Property: timeMode
+     */
+    private WidgetSettings.SolsticeEquinoxMode timeMode;
+    public WidgetSettings.SolsticeEquinoxMode timeMode()
+    {
+        return timeMode;
+    }
+    public void setTimeMode( WidgetSettings.SolsticeEquinoxMode mode )
+    {
+        timeMode = mode;
+    }
+
+    /**
+     * init from other SuntimesEquinoxSolsticeData object
+     * @param other
+     */
+    private void initFromOther( SuntimesEquinoxSolsticeData other )
+    {
+        initFromOther(other);
+        this.timeMode = other.timeMode();
+    }
+
+    /**
+     * init from shared preferences
+     * @param context
+     * @param appWidgetId
+     */
+    @Override
+    public void initFromSettings(Context context, int appWidgetId)
+    {
+        super.initFromSettings(context, appWidgetId);
+        timeMode = WidgetSettings.SolsticeEquinoxMode.EQUINOX_VERNAL;  // TODO
+        //timeMode = WidgetSettings.loadTimeModePref(context, appWidgetId);
+    }
+
+    /**
+     * result: eventCalendarThisYear
+     */
+    private Calendar eventCalendarThisYear;
+    public Calendar eventCalendarThisYear()
+    {
+        return eventCalendarThisYear;
+    }
+
+    /**
+     * result: eventCalendarOtherYear
+     */
+    private Calendar eventCalendarOtherYear;
+    public Calendar eventCalendarOtherYear()
+    {
+        return eventCalendarOtherYear;
+    }
+
+    public void calculate()
+    {
+        //Log.v("SuntimesWidgetData", "time mode: " + timeMode);
+        //Log.v("SuntimesWidgetData", "location_mode: " + locationMode.name());
+        //Log.v("SuntimesWidgetData", "latitude: " + location.getLatitude());
+        //Log.v("SuntimesWidgetData", "longitude: " + location.getLongitude());
+        //Log.v("SuntimesWidgetData", "timezone_mode: " + timezoneMode.name());
+        //Log.v("SuntimesWidgetData", "timezone: " + timezone);
+
+        SuntimesCalculatorFactory calculatorFactory = new SuntimesCalculatorFactory(context, calculatorMode);
+        SuntimesCalculator calculator = calculatorFactory.createCalculator(location, timezone);
+
+        todaysCalendar = Calendar.getInstance(TimeZone.getTimeZone(timezone));
+        otherCalendar = Calendar.getInstance(TimeZone.getTimeZone(timezone));
+
+        if (todayIsNotToday())
+        {
+            todaysCalendar.set(todayIs.get(Calendar.YEAR), todayIs.get(Calendar.MONTH), todayIs.get(Calendar.DAY_OF_MONTH));
+            otherCalendar.set(todayIs.get(Calendar.YEAR), todayIs.get(Calendar.MONTH), todayIs.get(Calendar.DAY_OF_MONTH));
+        }
+
+        otherCalendar.add(Calendar.YEAR, 1);
+
+        date = todaysCalendar.getTime();
+        dateOther = otherCalendar.getTime();
+
+        switch (timeMode)
+        {
+            case EQUINOX_VERNAL:
+                eventCalendarThisYear = calculator.getVernalEquinoxForYear(todaysCalendar);
+                eventCalendarOtherYear = calculator.getVernalEquinoxForYear(otherCalendar);
+                break;
+
+            case SOLSTICE_SUMMER:
+                eventCalendarThisYear = calculator.getSummerSolsticeForYear(todaysCalendar);
+                eventCalendarOtherYear = calculator.getSummerSolsticeForYear(otherCalendar);
+                break;
+
+            case EQUINOX_AUTUMNAL:
+                eventCalendarThisYear = calculator.getAutumnalEquinoxForYear(todaysCalendar);
+                eventCalendarOtherYear = calculator.getAutumnalEquinoxForYear(otherCalendar);
+                break;
+
+            case SOLSTICE_WINTER:
+            default:
+                eventCalendarThisYear = calculator.getWinterSolsticeForYear(todaysCalendar);
+                eventCalendarOtherYear = calculator.getWinterSolsticeForYear(otherCalendar);
+                break;
+        }
+
+        super.calculate();
+    }
+}
+
+
---
+++ app/src/main/java/com/forrestguice/suntimeswidget/calculator/SuntimesEquinoxSolsticeDataset.java
@@ -0,0 +1,124 @@
+/**
+    Copyright (C) 2014 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+package com.forrestguice.suntimeswidget.calculator;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+
+import com.forrestguice.suntimeswidget.settings.WidgetSettings;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class SuntimesEquinoxSolsticeDataset
+{
+    public SuntimesEquinoxSolsticeData dataEquinoxVernal;
+    public SuntimesEquinoxSolsticeData dataSolsticeSummer;
+    public SuntimesEquinoxSolsticeData dataEquinoxAutumnal;
+    public SuntimesEquinoxSolsticeData dataSolsticeWinter;
+
+    public SuntimesEquinoxSolsticeDataset(Context context)
+    {
+        dataEquinoxVernal = new SuntimesEquinoxSolsticeData(context, AppWidgetManager.INVALID_APPWIDGET_ID);
+        dataEquinoxVernal.setTimeMode(WidgetSettings.SolsticeEquinoxMode.EQUINOX_VERNAL);
+
+        dataSolsticeSummer = new SuntimesEquinoxSolsticeData(dataEquinoxVernal);
+        dataSolsticeSummer.setTimeMode(WidgetSettings.SolsticeEquinoxMode.SOLSTICE_SUMMER);
+
+        dataEquinoxAutumnal = new SuntimesEquinoxSolsticeData(dataEquinoxVernal);
+        dataEquinoxAutumnal.setTimeMode(WidgetSettings.SolsticeEquinoxMode.EQUINOX_AUTUMNAL);
+
+        dataSolsticeWinter = new SuntimesEquinoxSolsticeData(dataEquinoxVernal);
+        dataSolsticeWinter.setTimeMode(WidgetSettings.SolsticeEquinoxMode.SOLSTICE_WINTER);
+    }
+
+    public SuntimesEquinoxSolsticeDataset( SuntimesEquinoxSolsticeData dataEquinoxVernal,
+                                           SuntimesEquinoxSolsticeData dataSolsticeSummer,
+                                           SuntimesEquinoxSolsticeData dataEquinoxAutumnal,
+                                           SuntimesEquinoxSolsticeData dataSolsticeWinter )
+    {
+        this.dataEquinoxVernal = dataEquinoxVernal;
+        if (dataEquinoxVernal == null)
+        {
+            throw new NullPointerException("dataEquinoxVernal must not be null!");
+        }
+
+        this.dataSolsticeSummer = dataSolsticeSummer;
+        if (dataSolsticeSummer == null)
+        {
+            throw new NullPointerException("dataSolsticeSummer must not be null!");
+        }
+
+        this.dataEquinoxAutumnal = dataEquinoxAutumnal;
+        if (dataEquinoxAutumnal == null)
+        {
+            throw new NullPointerException("dataEquinoxAutumnal must not be null!");
+        }
+
+        this.dataSolsticeWinter = dataSolsticeWinter;
+        if (dataSolsticeWinter == null)
+        {
+            throw new NullPointerException("dataSolsticeWinter must not be null!");
+        }
+    }
+
+    public void calculateData()
+    {
+        dataEquinoxVernal.calculate();
+        dataSolsticeSummer.calculate();
+        dataEquinoxAutumnal.calculate();
+        dataSolsticeWinter.calculate();
+    }
+
+    public boolean isCalculated()
+    {
+        return dataEquinoxVernal.isCalculated();
+    }
+
+    public void invalidateCalculation()
+    {
+        dataEquinoxVernal.invalidateCalculation();
+        dataSolsticeSummer.invalidateCalculation();
+        dataEquinoxAutumnal.invalidateCalculation();
+        dataSolsticeWinter.invalidateCalculation();
+    }
+
+    public Calendar todayIs()
+    {
+        return dataEquinoxVernal.todayIs();
+    }
+
+    public boolean todayIsNotToday()
+    {
+        return dataEquinoxVernal.todayIsNotToday();
+    }
+
+    public String timezone()
+    {
+        return dataEquinoxVernal.timezone();
+    }
+
+    public Calendar now()
+    {
+        return Calendar.getInstance(TimeZone.getTimeZone(timezone()));
+    }
+}
+
+
