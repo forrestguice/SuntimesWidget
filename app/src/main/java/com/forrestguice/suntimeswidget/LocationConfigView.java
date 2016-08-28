c234390a132fee37af79b3f6e3bc72c692295bba
Forrest Guice <forrestguice@gmail.com>
2016-08-27 23:39:56 -0700

    layout rtl fixes
    right-to-left layout fixes

--- app/src/main/java/com/forrestguice/suntimeswidget/LocationConfigView.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/LocationConfigView.java
@@ -310,7 +310,7 @@ public class LocationConfigView extends LinearLayout
 
         String[] from = new String[] {"name"};
         int[] to = new int[] {android.R.id.text1};
-        getFixAdapter = new SimpleCursorAdapter(myParent, android.R.layout.simple_spinner_item, null, from, to);
+        getFixAdapter = new SimpleCursorAdapter(myParent, R.layout.layout_listitem_locations, null, from, to);
         getFixAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spin_locationName = (Spinner)findViewById(R.id.appwidget_location_nameSelect);
