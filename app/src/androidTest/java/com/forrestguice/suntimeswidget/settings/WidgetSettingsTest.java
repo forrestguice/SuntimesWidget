6eb2e6b35c500d480a3696778017219b0f194711
Forrest Guice <forrestguice@gmail.com>
2017-04-16 11:15:17 -0700

    add test

---
+++ app/src/androidTest/java/com/forrestguice/suntimeswidget/settings/WidgetSettingsTest.java
@@ -0,0 +1,34 @@
+/**
+    Copyright (C) 2017 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.forrestguice.suntimeswidget.settings;
+
+import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.forrestguice.suntimeswidget.SuntimesActivityTestBase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class WidgetSettingsTest extends SuntimesActivityTestBase
+{
+}
