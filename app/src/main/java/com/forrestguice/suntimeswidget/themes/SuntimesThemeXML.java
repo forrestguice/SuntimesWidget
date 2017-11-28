b63ed40cd3b70a42d5a53a17371a266194161974
Forrest Guice <forrestguice@gmail.com>
2017-11-28 14:25:00 -0700

    export themes
    bump version field

--- app/src/main/java/com/forrestguice/suntimeswidget/themes/SuntimesThemeXML.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/themes/SuntimesThemeXML.java
@@ -37,7 +37,7 @@ public class SuntimesThemeXML implements SuntimesThemeIO
 {
     public static final String KEY_THEMES = "themes";
     public static final String KEY_THEME = "theme";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public SuntimesThemeXML() {}
 
