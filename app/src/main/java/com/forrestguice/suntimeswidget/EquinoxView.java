a1b1ff322dadae09aeaa6f0568ce4e4f6764ebbe
Forrest Guice <forrestguice@gmail.com>
2017-12-12 01:17:51 -0700

    lint: newApi
    requires 11, current minimum 10

--- app/src/main/java/com/forrestguice/suntimeswidget/EquinoxView.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/EquinoxView.java
@@ -82,12 +82,6 @@ public class EquinoxView extends LinearLayout
         init(context, attrs);
     }
 
-    public EquinoxView(Context context, AttributeSet attrs, int defStyle)
-    {
-        super(context, attrs, defStyle);
-        init(context, attrs);
-    }
-
     private void init(Context context, AttributeSet attrs)
     {
         initLocale(context);
