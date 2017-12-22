1a192138a9cc79717fb7063dd50c661c21387dac
Forrest Guice <forrestguice@users.noreply.github.com>
2017-12-21 22:58:20 -0700

    trackingMode pref
    add app pref

--- app/src/main/java/com/forrestguice/suntimeswidget/SuntimesSettingsActivity.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/SuntimesSettingsActivity.java
@@ -237,6 +237,14 @@ public class SuntimesSettingsActivity extends PreferenceActivity implements Shar
             updateLocale();
             return;
         }
+
+        if (key.endsWith(WidgetSettings.PREF_KEY_GENERAL_TRACKINGMODE))
+        {
+            // the pref activity saves to: com.forrestguice.suntimeswidget_preferences.xml,
+            // ...but this is a widget setting (belongs in com.forrestguice.suntimeswidget.xml)
+            WidgetSettings.saveTrackingModePref(this, 0, WidgetSettings.TrackingMode.valueOf(sharedPreferences.getString(key, "missing")));
+            return;
+        }
     }
 
     protected void updateLocale()
