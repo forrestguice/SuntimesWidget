ca6013f1970902751855dcf43f8bb3144329fd26
Forrest Guice <forrestguice@users.noreply.github.com>
2018-01-14 20:37:20 -0700

    show durations
    show twilight,day,night durations in lightmap dialog

--- app/src/main/java/com/forrestguice/suntimeswidget/LightMapDialog.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/LightMapDialog.java
@@ -1,5 +1,5 @@
 /**
-    Copyright (C) 2017 Forrest Guice
+    Copyright (C) 2017-2018 Forrest Guice
     This file is part of SuntimesWidget.
 
     SuntimesWidget is free software: you can redistribute it and/or modify
@@ -33,13 +33,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.forrestguice.suntimeswidget.calculator.SuntimesRiseSetData;
 import com.forrestguice.suntimeswidget.calculator.SuntimesRiseSetDataset;
+import com.forrestguice.suntimeswidget.settings.AppSettings;
+import com.forrestguice.suntimeswidget.settings.WidgetSettings;
 
 public class LightMapDialog extends DialogFragment
 {
+    private static SuntimesUtils utils = new SuntimesUtils();
+
     private LightMapView lightmap;
     private ImageView ic_night, ic_astro, ic_nautical, ic_civil, ic_day;
+    private TextView txt_night, txt_astro, txt_nautical, txt_civil, txt_day;
 
     private SuntimesRiseSetDataset data;
     public void setData( SuntimesRiseSetDataset data )
@@ -72,7 +79,7 @@ public class LightMapDialog extends DialogFragment
             @Override
             public void onShow(DialogInterface dialogInterface)
             {
-                lightmap.updateViews(data);
+                updateViews(data);
             }
         });
 
@@ -83,11 +90,21 @@ public class LightMapDialog extends DialogFragment
     public void initViews(View dialogView)
     {
         lightmap = (LightMapView)dialogView.findViewById(R.id.info_time_lightmap);
+
         ic_night = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_night_icon);
+        txt_night = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_night_duration);
+
         ic_astro = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_astro_icon);
+        txt_astro = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_astro_duration);
+
         ic_nautical = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_nautical_icon);
+        txt_nautical = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_nautical_duration);
+
         ic_civil = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_civil_icon);
+        txt_civil = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_civil_duration);
+
         ic_day = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_day_icon);
+        txt_day = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_day_duration);
     }
 
     @SuppressWarnings("ResourceType")
@@ -129,8 +146,63 @@ public class LightMapDialog extends DialogFragment
     {
         if (lightmap != null)
         {
+            Context context = getContext();
+            boolean showSeconds = WidgetSettings.loadShowSecondsPref(context, 0);
+
+            String displayDay = "";
+            long lengthDay = data.dayLength();
+            if (lengthDay > 0)
+            {
+                SuntimesUtils.TimeDisplayText txtDay = utils.timeDeltaLongDisplayString(lengthDay, showSeconds);
+                displayDay = context.getString(R.string.length_twilight1, txtDay.toString());
+            }
+            txt_day.setText(displayDay);
+
+            String displayCivil = "";
+            long lengthCivil0 = data.civilMorningLength();
+            long lengthCivil1 = data.civilEveningLength();
+            if (lengthCivil0 > 0 && lengthCivil1 > 0)
+            {
+                SuntimesUtils.TimeDisplayText txtCivil0 = utils.timeDeltaLongDisplayString(lengthCivil0, showSeconds);
+                SuntimesUtils.TimeDisplayText txtCivil1 = utils.timeDeltaLongDisplayString(lengthCivil1, showSeconds);
+                displayCivil = context.getString(R.string.length_twilight2, txtCivil0.toString(), txtCivil1.toString());
+            }
+            txt_civil.setText(displayCivil);
+
+            String displayNautical = "";
+            long lengthNautical0 = data.nauticalMorningLength();
+            long lengthNautical1 = data.nauticalEveningLength();
+            if (lengthNautical0 > 0 && lengthNautical1 > 0)
+            {
+                SuntimesUtils.TimeDisplayText txtNautical0 = utils.timeDeltaLongDisplayString(lengthNautical0, showSeconds);
+                SuntimesUtils.TimeDisplayText txtNautical1 = utils.timeDeltaLongDisplayString(lengthNautical1, showSeconds);
+                displayNautical = context.getString(R.string.length_twilight2, txtNautical0.toString(), txtNautical1.toString());
+            }
+            txt_nautical.setText(displayNautical);
+
+            String displayAstro = "";
+            long lengthAstro0 = data.astroMorningLength();
+            long lengthAstro1 = data.astroEveningLength();
+            if (lengthAstro0 > 0 && lengthAstro1 > 0)
+            {
+                SuntimesUtils.TimeDisplayText txtAstro0 = utils.timeDeltaLongDisplayString(lengthAstro0, showSeconds);
+                SuntimesUtils.TimeDisplayText txtAstro1 = utils.timeDeltaLongDisplayString(lengthAstro1, showSeconds);
+                displayAstro = context.getString(R.string.length_twilight2, txtAstro0.toString(), txtAstro1.toString());
+            }
+            txt_astro.setText(displayAstro);
+
+            String displayNight = "";
+            long lengthNight = data.nightLength();
+            if (lengthNight > 0)
+            {
+                SuntimesUtils.TimeDisplayText txtNight = utils.timeDeltaLongDisplayString(lengthNight, showSeconds);
+                displayNight = context.getString(R.string.length_twilight1, txtNight.toString());
+            }
+            txt_night.setText(displayNight);
+
             lightmap.updateViews(data);
             Log.d("DEBUG", "LightMapDialog updated");
         }
     }
+
 }
