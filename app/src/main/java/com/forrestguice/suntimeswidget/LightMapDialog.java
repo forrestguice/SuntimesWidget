3cb689898e1f725654e1dc9cba32609013322f0b
Forrest Guice <forrestguice@users.noreply.github.com>
2018-01-15 17:44:47 -0700

    refactor

--- app/src/main/java/com/forrestguice/suntimeswidget/LightMapDialog.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/LightMapDialog.java
@@ -29,24 +29,26 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.text.SpannableString;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.forrestguice.suntimeswidget.calculator.SuntimesRiseSetData;
 import com.forrestguice.suntimeswidget.calculator.SuntimesRiseSetDataset;
-import com.forrestguice.suntimeswidget.settings.AppSettings;
 import com.forrestguice.suntimeswidget.settings.WidgetSettings;
+
+import java.util.ArrayList;
 
 public class LightMapDialog extends DialogFragment
 {
     private static SuntimesUtils utils = new SuntimesUtils();
 
     private LightMapView lightmap;
-    private ImageView ic_night, ic_astro, ic_nautical, ic_civil, ic_day;
-    private TextView txt_night, txt_astro, txt_nautical, txt_civil, txt_day;
+    private LightMapKey field_night, field_astro, field_nautical, field_civil, field_day;
+    private int colorNight, colorAstro, colorNautical, colorCivil, colorDay;
+    private boolean showSeconds = false;
 
     private SuntimesRiseSetDataset data;
     public void setData( SuntimesRiseSetDataset data )
@@ -90,21 +92,11 @@ public class LightMapDialog extends DialogFragment
     public void initViews(View dialogView)
     {
         lightmap = (LightMapView)dialogView.findViewById(R.id.info_time_lightmap);
-
-        ic_night = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_night_icon);
-        txt_night = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_night_duration);
-
-        ic_astro = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_astro_icon);
-        txt_astro = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_astro_duration);
-
-        ic_nautical = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_nautical_icon);
-        txt_nautical = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_nautical_duration);
-
-        ic_civil = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_civil_icon);
-        txt_civil = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_civil_duration);
-
-        ic_day = (ImageView)dialogView.findViewById(R.id.info_time_lightmap_key_day_icon);
-        txt_day = (TextView)dialogView.findViewById(R.id.info_time_lightmap_key_day_duration);
+        field_night = new LightMapKey(dialogView, R.id.info_time_lightmap_key_night_icon, R.id.info_time_lightmap_key_night_label, R.id.info_time_lightmap_key_night_duration);
+        field_astro = new LightMapKey(dialogView, R.id.info_time_lightmap_key_astro_icon, R.id.info_time_lightmap_key_astro_label, R.id.info_time_lightmap_key_astro_duration);
+        field_nautical = new LightMapKey(dialogView, R.id.info_time_lightmap_key_nautical_icon, R.id.info_time_lightmap_key_nautical_label, R.id.info_time_lightmap_key_nautical_duration);
+        field_civil = new LightMapKey(dialogView, R.id.info_time_lightmap_key_civil_icon, R.id.info_time_lightmap_key_civil_label, R.id.info_time_lightmap_key_civil_duration);
+        field_day = new LightMapKey(dialogView, R.id.info_time_lightmap_key_day_icon, R.id.info_time_lightmap_key_day_label, R.id.info_time_lightmap_key_day_duration);
     }
 
     @SuppressWarnings("ResourceType")
@@ -118,23 +110,23 @@ public class LightMapDialog extends DialogFragment
         };
         TypedArray typedArray = context.obtainStyledAttributes(colorAttrs);
         int def = R.color.transparent;
-        int colorNight = ContextCompat.getColor(context, typedArray.getResourceId(0, def));
-        int colorAstro = ContextCompat.getColor(context, typedArray.getResourceId(1, def));
-        int colorNautical = ContextCompat.getColor(context, typedArray.getResourceId(2, def));
-        int colorCivil = ContextCompat.getColor(context, typedArray.getResourceId(3, def));
-        int colorDay = ContextCompat.getColor(context, typedArray.getResourceId(4, def));
+        colorNight = ContextCompat.getColor(context, typedArray.getResourceId(0, def));
+        colorAstro = ContextCompat.getColor(context, typedArray.getResourceId(1, def));
+        colorNautical = ContextCompat.getColor(context, typedArray.getResourceId(2, def));
+        colorCivil = ContextCompat.getColor(context, typedArray.getResourceId(3, def));
+        colorDay = ContextCompat.getColor(context, typedArray.getResourceId(4, def));
         typedArray.recycle();
 
-        colorizeImageView(ic_night, colorNight);
-        colorizeImageView(ic_astro, colorAstro);
-        colorizeImageView(ic_nautical, colorNautical);
-        colorizeImageView(ic_civil, colorCivil);
-        colorizeImageView(ic_day, colorDay);
+        colorizeImageView(field_night.icon, colorNight);
+        colorizeImageView(field_astro.icon, colorAstro);
+        colorizeImageView(field_nautical.icon, colorNautical);
+        colorizeImageView(field_civil.icon, colorCivil);
+        colorizeImageView(field_day.icon, colorDay);
     }
 
     private void colorizeImageView(ImageView view, int color)
     {
-        if (view.getBackground() != null)
+        if (view != null && view.getBackground() != null)
         {
             GradientDrawable d = (GradientDrawable) view.getBackground().mutate();
             d.setColor(color);
@@ -147,61 +139,80 @@ public class LightMapDialog extends DialogFragment
         if (lightmap != null)
         {
             Context context = getContext();
-            boolean showSeconds = WidgetSettings.loadShowSecondsPref(context, 0);
-
-            String displayDay = "";
-            long lengthDay = data.dayLength();
-            if (lengthDay > 0)
-            {
-                SuntimesUtils.TimeDisplayText txtDay = utils.timeDeltaLongDisplayString(lengthDay, showSeconds);
-                displayDay = context.getString(R.string.length_twilight1, txtDay.toString());
-            }
-            txt_day.setText(displayDay);
-
-            String displayCivil = "";
-            long lengthCivil0 = data.civilMorningLength();
-            long lengthCivil1 = data.civilEveningLength();
-            if (lengthCivil0 > 0 && lengthCivil1 > 0)
-            {
-                SuntimesUtils.TimeDisplayText txtCivil0 = utils.timeDeltaLongDisplayString(lengthCivil0, showSeconds);
-                SuntimesUtils.TimeDisplayText txtCivil1 = utils.timeDeltaLongDisplayString(lengthCivil1, showSeconds);
-                displayCivil = context.getString(R.string.length_twilight2, txtCivil0.toString(), txtCivil1.toString());
-            }
-            txt_civil.setText(displayCivil);
-
-            String displayNautical = "";
-            long lengthNautical0 = data.nauticalMorningLength();
-            long lengthNautical1 = data.nauticalEveningLength();
-            if (lengthNautical0 > 0 && lengthNautical1 > 0)
-            {
-                SuntimesUtils.TimeDisplayText txtNautical0 = utils.timeDeltaLongDisplayString(lengthNautical0, showSeconds);
-                SuntimesUtils.TimeDisplayText txtNautical1 = utils.timeDeltaLongDisplayString(lengthNautical1, showSeconds);
-                displayNautical = context.getString(R.string.length_twilight2, txtNautical0.toString(), txtNautical1.toString());
-            }
-            txt_nautical.setText(displayNautical);
-
-            String displayAstro = "";
-            long lengthAstro0 = data.astroMorningLength();
-            long lengthAstro1 = data.astroEveningLength();
-            if (lengthAstro0 > 0 && lengthAstro1 > 0)
-            {
-                SuntimesUtils.TimeDisplayText txtAstro0 = utils.timeDeltaLongDisplayString(lengthAstro0, showSeconds);
-                SuntimesUtils.TimeDisplayText txtAstro1 = utils.timeDeltaLongDisplayString(lengthAstro1, showSeconds);
-                displayAstro = context.getString(R.string.length_twilight2, txtAstro0.toString(), txtAstro1.toString());
-            }
-            txt_astro.setText(displayAstro);
-
-            String displayNight = "";
-            long lengthNight = data.nightLength();
-            if (lengthNight > 0)
-            {
-                SuntimesUtils.TimeDisplayText txtNight = utils.timeDeltaLongDisplayString(lengthNight, showSeconds);
-                displayNight = context.getString(R.string.length_twilight1, txtNight.toString());
-            }
-            txt_night.setText(displayNight);
-
+            showSeconds = WidgetSettings.loadShowSecondsPref(context, 0);
+            field_day.updateInfo(context, new long[] {data.dayLength()});
+            field_civil.updateInfo(context, data.civilTwilightLength());
+            field_nautical.updateInfo(context, data.nauticalTwilightLength());
+            field_astro.updateInfo(context, data.astroTwilightLength());
+            field_night.updateInfo(context, new long[] {data.nightLength()});
             lightmap.updateViews(data);
             Log.d("DEBUG", "LightMapDialog updated");
+        }
+    }
+
+    /**
+     * LightMapKey
+     */
+    private class LightMapKey
+    {
+        protected ImageView icon;
+        protected TextView label;
+        protected TextView info;
+
+        public LightMapKey(ImageView icon, TextView label, TextView duration)
+        {
+            this.icon = icon;
+            this.label = label;
+            this.info = duration;
+        }
+
+        public LightMapKey(@NonNull View parent, int iconRes, int labelRes, int durationRes)
+        {
+            icon = (ImageView)parent.findViewById(iconRes);
+            label = (TextView)parent.findViewById(labelRes);
+            info = (TextView)parent.findViewById(durationRes);
+        }
+
+        public void setVisible(boolean visible)
+        {
+            int visibility = (visible ? View.VISIBLE : View.GONE);
+            if (label != null) {
+                label.setVisibility(visibility);
+            }
+            if (info != null) {
+                info.setVisibility(visibility);
+            }
+            if (icon != null) {
+                icon.setVisibility(visibility);
+            }
+        }
+
+        public void updateInfo(Context context, long[] durations)
+        {
+            if (info == null)
+                return;
+
+            ArrayList<SuntimesUtils.TimeDisplayText> txt = new ArrayList<>();
+            for (int i=0; i<durations.length; i++)
+            {
+                if (durations[i] > 0)
+                    txt.add(utils.timeDeltaLongDisplayString(durations[i], showSeconds));
+            }
+
+            if (txt.size() == 1)
+            {
+                info.setText(new SpannableString(context.getString(R.string.length_twilight1, txt.get(0).toString())));
+                setVisible(true);
+
+            } else if (txt.size() >= 2) {
+                String s = context.getString(R.string.length_twilight2, txt.get(0).toString(), txt.get(1).toString());
+                info.setText(SuntimesUtils.createBoldColorSpan(s, "|", colorDay));
+                setVisible(true);
+
+            } else {
+                info.setText(new SpannableString(""));
+                setVisible(false);
+            }
         }
     }
 
