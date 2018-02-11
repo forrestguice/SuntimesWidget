8444bc3b65b12ca0b5520e217771e03724babfce
Forrest Guice <forrestguice@users.noreply.github.com>
2018-02-11 00:30:04 -0700

    widget

--- app/src/main/java/com/forrestguice/suntimeswidget/layouts/MoonLayout_3x1_0.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/layouts/MoonLayout_3x1_0.java
@@ -19,24 +19,17 @@
 package com.forrestguice.suntimeswidget.layouts;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Build;
-import android.text.SpannableString;
 import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.View;
 import android.widget.RemoteViews;
 
 import com.forrestguice.suntimeswidget.R;
 import com.forrestguice.suntimeswidget.SuntimesUtils;
-import com.forrestguice.suntimeswidget.calculator.MoonPhaseDisplay;
 import com.forrestguice.suntimeswidget.calculator.SuntimesCalculator;
 import com.forrestguice.suntimeswidget.calculator.SuntimesMoonData;
 import com.forrestguice.suntimeswidget.settings.WidgetSettings;
 import com.forrestguice.suntimeswidget.themes.SuntimesTheme;
 
-import java.text.NumberFormat;
 import java.util.Calendar;
 
 public class MoonLayout_3x1_0 extends MoonLayout
@@ -61,113 +54,76 @@ public class MoonLayout_3x1_0 extends MoonLayout
     public void updateViews(Context context, int appWidgetId, RemoteViews views, SuntimesMoonData data)
     {
         super.updateViews(context, appWidgetId, views, data);
+        boolean showSeconds = WidgetSettings.loadShowSecondsPref(context, appWidgetId);
 
-        SuntimesUtils.TimeDisplayText newMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.NEW));
+        SuntimesUtils.TimeDisplayText newMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.NEW), showSeconds);
         views.setTextViewText(R.id.moonphase_new_date, newMoonString.getValue());
 
-        SuntimesUtils.TimeDisplayText firstQuarterMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.FIRST_QUARTER));
+        SuntimesUtils.TimeDisplayText firstQuarterMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.FIRST_QUARTER), showSeconds);
         views.setTextViewText(R.id.moonphase_firstquarter_date, firstQuarterMoonString.getValue());
 
-        SuntimesUtils.TimeDisplayText fullMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.FULL));
+        SuntimesUtils.TimeDisplayText fullMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.FULL), showSeconds);
         views.setTextViewText(R.id.moonphase_full_date, fullMoonString.getValue());
 
-        SuntimesUtils.TimeDisplayText thirdQuarterMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.THIRD_QUARTER));
+        SuntimesUtils.TimeDisplayText thirdQuarterMoonString = utils.calendarDateTimeDisplayString(context, data.moonPhaseCalendar(SuntimesCalculator.MoonPhase.THIRD_QUARTER), showSeconds);
         views.setTextViewText(R.id.moonphase_thirdquarter_date, thirdQuarterMoonString.getValue());
-
-        /**boolean showSeconds = WidgetSettings.loadShowSecondsPref(context, appWidgetId);
-
-        SuntimesUtils.TimeDisplayText riseString = utils.calendarTimeShortDisplayString(context, data.moonriseCalendarToday(), showSeconds);
-        views.setTextViewText(R.id.text_time_moonrise, riseString.getValue());
-        views.setTextViewText(R.id.text_time_moonrise_suffix, riseString.getSuffix());
-
-        SuntimesUtils.TimeDisplayText setString = utils.calendarTimeShortDisplayString(context, data.moonsetCalendarToday(), showSeconds);
-        views.setTextViewText(R.id.text_time_moonset, setString.getValue());
-        views.setTextViewText(R.id.text_time_moonset_suffix, setString.getSuffix());
-
-        NumberFormat percentage = NumberFormat.getPercentInstance();
-        String illum = percentage.format(data.getMoonIlluminationToday());
-        String illumNote = context.getString(R.string.moon_illumination, illum);
-        SpannableString illumNoteSpan = SuntimesUtils.createColorSpan(illumNote, illum, illumColor);
-        views.setTextViewText(R.id.text_info_moonillum, illumNoteSpan);
-
-        for (MoonPhaseDisplay moonPhase : MoonPhaseDisplay.values())
-        {
-            views.setViewVisibility(moonPhase.getView(), View.GONE);
-        }
-
-        MoonPhaseDisplay phase = data.getMoonPhaseToday();
-        if (phase != null)
-        {
-            views.setTextViewText(R.id.text_info_moonphase, phase.getLongDisplayString());
-            views.setViewVisibility(phase.getView(), View.VISIBLE);
-        }*/
     }
-
-    private int illumColor = Color.WHITE;
 
     @Override
     public void themeViews(Context context, RemoteViews views, SuntimesTheme theme)
     {
         super.themeViews(context, views, theme);
 
-        /**illumColor = theme.getTimeColor();
-        int textColor = theme.getTextColor();
-        views.setTextColor(R.id.text_info_moonillum, textColor);
-        views.setTextColor(R.id.text_info_moonphase, textColor);
-
         int moonriseColor = theme.getMoonriseTextColor();
-        int suffixColor = theme.getTimeSuffixColor();
-        views.setTextColor(R.id.text_time_moonrise_suffix, suffixColor);
-        views.setTextColor(R.id.text_time_moonrise, moonriseColor);
+        //int moonsetColor = theme.getMoonsetTextColor();
+        //int suffixColor = theme.getTimeSuffixColor();
 
-        int moonsetColor = theme.getMoonsetTextColor();
-        views.setTextColor(R.id.text_time_moonset_suffix, suffixColor);
-        views.setTextColor(R.id.text_time_moonset, moonsetColor);
+        views.setTextColor(R.id.moonphase_new_date, moonriseColor);
+        views.setTextColor(R.id.moonphase_firstquarter_date, moonriseColor);
+        views.setTextColor(R.id.moonphase_full_date, moonriseColor);
+        views.setTextColor(R.id.moonphase_thirdquarter_date, moonriseColor);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
         {
             float timeSize = theme.getTimeSizeSp();
-            float suffSize = theme.getTimeSuffixSizeSp();
-            float textSize = theme.getTextSizeSp();
+            //float suffSize = theme.getTimeSuffixSizeSp();
+            //float textSize = theme.getTextSizeSp();
 
-            views.setTextViewTextSize(R.id.text_info_moonphase, TypedValue.COMPLEX_UNIT_SP, textSize);
-            views.setTextViewTextSize(R.id.text_info_moonillum, TypedValue.COMPLEX_UNIT_SP, textSize);
-
-            views.setTextViewTextSize(R.id.text_time_moonrise_suffix, TypedValue.COMPLEX_UNIT_SP, suffSize);
-            views.setTextViewTextSize(R.id.text_time_moonrise, TypedValue.COMPLEX_UNIT_SP, timeSize);
-
-            views.setTextViewTextSize(R.id.text_time_moonset, TypedValue.COMPLEX_UNIT_SP, timeSize);
-            views.setTextViewTextSize(R.id.text_time_moonset_suffix, TypedValue.COMPLEX_UNIT_SP, suffSize);
+            views.setTextViewTextSize(R.id.moonphase_new_date, TypedValue.COMPLEX_UNIT_SP, timeSize);
+            views.setTextViewTextSize(R.id.moonphase_firstquarter_date, TypedValue.COMPLEX_UNIT_SP, timeSize);
+            views.setTextViewTextSize(R.id.moonphase_full_date, TypedValue.COMPLEX_UNIT_SP, timeSize);
+            views.setTextViewTextSize(R.id.moonphase_thirdquarter_date, TypedValue.COMPLEX_UNIT_SP, timeSize);
         }
-
-        Bitmap moonriseIcon = SuntimesUtils.insetDrawableToBitmap(context, R.drawable.ic_moon_rise, moonriseColor, moonriseColor, 0);
-        views.setImageViewBitmap(R.id.icon_time_moonrise, moonriseIcon);
-
-        Bitmap moonsetIcon = SuntimesUtils.insetDrawableToBitmap(context, R.drawable.ic_moon_set, moonsetColor,moonsetColor, 0);
-        views.setImageViewBitmap(R.id.icon_time_moonset, moonsetIcon);*/
     }
 
     @Override
     public void prepareForUpdate(SuntimesMoonData data)
     {
-        /**Calendar riseTime = data.moonriseCalendarToday();
-        Calendar setTime = data.moonsetCalendarToday();
+        Calendar startOfDay = (Calendar)data.calendar().clone();
+        startOfDay.set(Calendar.HOUR_OF_DAY, 0);
+        startOfDay.set(Calendar.MINUTE, 0);
+        startOfDay.set(Calendar.SECOND, 0);
 
-        if (riseTime != null && setTime != null)
+        SuntimesCalculator.MoonPhase nextPhase = data.nextPhase(startOfDay);
+        switch (nextPhase)
         {
-            if (riseTime.before(setTime))
-                this.layoutID = R.layout.layout_widget_moon_2x1_0;      // moon rises then sets
-            else this.layoutID = R.layout.layout_widget_moon_2x1_01;    // moon sets then rises
+            case THIRD_QUARTER:
+                this.layoutID = R.layout.layout_widget_moon_3x1_03;
+                break;
+                
+            case FULL:
+                this.layoutID = R.layout.layout_widget_moon_3x1_02;
+                break;
 
-        } else if (riseTime == null && setTime == null) {
-            this.layoutID = R.layout.layout_widget_moon_2x1_0;  // moon doesn't rise or set today
+            case FIRST_QUARTER:
+                this.layoutID = R.layout.layout_widget_moon_3x1_01;
+                break;
 
-        } else if (setTime != null) {
-            this.layoutID = R.layout.layout_widget_moon_2x1_01;  // moon doesn't rise (but it sets)
-
-        } else {
-            this.layoutID = R.layout.layout_widget_moon_2x1_0;  // moon doesn't set (but it rises)
-        }*/
+            case NEW:
+            default:
+                this.layoutID = R.layout.layout_widget_moon_3x1_0;
+                break;
+        }
     }
 }
 
