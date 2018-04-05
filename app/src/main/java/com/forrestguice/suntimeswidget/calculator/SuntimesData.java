forrestguice/SuntimesWidget

commit 273e0d77a648c43c83562955219b6339b14ed6a0

refactor

app/src/main/java/com/forrestguice/suntimeswidget/calculator/SuntimesData.java
@@ -271,6 +271,19 @@ public class SuntimesData
         return Calendar.getInstance(TimeZone.getTimeZone(timezone().getID()));
     }
 
+    /**
+     * @param date
+     * @return
+     */
+    public Calendar nowThen(Calendar date)
+    {
+        Calendar nowThen = now();
+        nowThen.set(Calendar.YEAR, date.get(Calendar.YEAR));
+        nowThen.set(Calendar.MONTH, date.get(Calendar.MONTH));
+        nowThen.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH));
+        return nowThen;
+    }
+
 }
 
 

app/src/main/java/com/forrestguice/suntimeswidget/calculator/SuntimesRiseSetDataset.java
@@ -219,16 +219,12 @@ public class SuntimesRiseSetDataset
 
     public Calendar now()
     {
-        return Calendar.getInstance(timezone());
+        return dataActual.now();
     }
 
     public Calendar nowThen(Calendar date)
     {
-        Calendar nowThen = now();
-        nowThen.set(Calendar.YEAR, date.get(Calendar.YEAR));
-        nowThen.set(Calendar.MONTH, date.get(Calendar.MONTH));
-        nowThen.set(Calendar.DAY_OF_MONTH, date.get(Calendar.DAY_OF_MONTH));
-        return nowThen;
+        return dataActual.nowThen(date);
     }
 
     public static Calendar midnight(Calendar date)

commit 1feffd3ad5f97a5b9cbeb24dd14e7c975cfc19e5

now
fixes bug in solar time mode introduced by commit 273e0d77a648c43c83562955219b6339b14ed6a0

app/src/main/java/com/forrestguice/suntimeswidget/calculator/SuntimesData.java
@@ -268,7 +268,7 @@ public class SuntimesData
      */
     public Calendar now()
     {
-        return Calendar.getInstance(TimeZone.getTimeZone(timezone().getID()));
+        return Calendar.getInstance(timezone());
     }
 
     /**