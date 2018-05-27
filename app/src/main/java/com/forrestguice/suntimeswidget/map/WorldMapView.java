254aa408e82756b7757fcf5b79a8e82d822d9b0f
Forrest Guice <forrestguice@users.noreply.github.com>
2018-05-27 13:19:19 -0700

    worldMapView
    draw background

--- app/src/main/java/com/forrestguice/suntimeswidget/map/WorldMapView.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/map/WorldMapView.java
@@ -17,8 +17,14 @@
 */
 package com.forrestguice.suntimeswidget.map;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -26,13 +32,18 @@ import android.util.AttributeSet;
 import android.util.Log;
 
 import com.forrestguice.suntimeswidget.R;
+import com.forrestguice.suntimeswidget.calculator.SuntimesCalculator;
 import com.forrestguice.suntimeswidget.calculator.SuntimesRiseSetDataset;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class WorldMapView extends android.support.v7.widget.AppCompatImageView
 {
     public static final int DEFAULT_MAX_UPDATE_RATE = 15 * 1000;  // ms value; once every 15s
 
     private WorldMapTask drawTask;
+    private Drawable background;
 
     private int maxUpdateRate = DEFAULT_MAX_UPDATE_RATE;
 
@@ -60,8 +71,10 @@ public class WorldMapView extends android.support.v7.widget.AppCompatImageView
     {
         if (isInEditMode())
         {
-            setBackgroundColor(ContextCompat.getColor(context, R.color.graphColor_night_dark));
+            setBackgroundColor(Color.WHITE);
         }
+
+        background = ContextCompat.getDrawable(context, R.drawable.world_map_blank_without_borders);
     }
 
     public int getMaxUpdateRate()
@@ -121,7 +134,7 @@ public class WorldMapView extends android.support.v7.widget.AppCompatImageView
         {
             drawTask.cancel(true);
         }
-        drawTask = new WorldMapTask();
+        drawTask = new WorldMapTask(getContext());
         drawTask.setListener(new WorldMapTaskListener()
         {
             @Override
@@ -130,7 +143,7 @@ public class WorldMapView extends android.support.v7.widget.AppCompatImageView
                 setImageBitmap(result);
             }
         });
-        drawTask.execute(data, getWidth(), getHeight());
+        drawTask.execute(data, background.getIntrinsicWidth(), background.getIntrinsicHeight(), background);
     }
 
     /**
@@ -180,10 +193,45 @@ public class WorldMapView extends android.support.v7.widget.AppCompatImageView
      */
     public static class WorldMapTask extends AsyncTask<Object, Void, Bitmap>
     {
+        private Drawable map;
+        private int shadowColor = Color.GRAY;
+        private int backgroundColor = Color.GREEN;
+
+        private int sunFillColor, sunStrokeColor;
+        private int sunRadius = 8;
+        private int sunStroke = 2;
+
+        private int moonFillColor, moonStrokeColor;
+        private int moonRadius = 8;
+        private int moonStroke = 2;
+
+        @SuppressLint("ResourceType")
+        public WorldMapTask(Context context)
+        {
+            int[] colorAttrs = {
+                    R.attr.graphColor_pointFill,            // 0
+                    R.attr.graphColor_pointStroke,          // 1
+                    R.attr.moonriseColor,                   // 2
+                    R.attr.moonsetColor                     // 3
+            };
+            TypedArray typedArray = context.obtainStyledAttributes(colorAttrs);
+            int def = R.color.transparent;
+
+            sunFillColor = ContextCompat.getColor(context, typedArray.getResourceId(0, def));
+            sunStrokeColor = ContextCompat.getColor(context, typedArray.getResourceId(1, def));
+            moonFillColor = ContextCompat.getColor(context, typedArray.getResourceId(2, def));
+            moonStrokeColor = ContextCompat.getColor(context, typedArray.getResourceId(3, def));
+
+            typedArray.recycle();
+
+            shadowColor = ContextCompat.getColor(context, R.color.card_bg_darktrans);
+        }
+
         /**
          * @param params 0: SuntimesRiseSetDataset,
          *               1: Integer (width),
-         *               2: Integer (height)
+         *               2: Integer (height),
+         *               3: Drawable (map)
          * @return a bitmap, or null params are invalid
          */
         @Override
@@ -195,6 +243,7 @@ public class WorldMapView extends android.support.v7.widget.AppCompatImageView
                 data = (SuntimesRiseSetDataset)params[0];
                 w = (Integer)params[1];
                 h = (Integer)params[2];
+                map = (Drawable)params[3];
 
             } catch (ClassCastException e) {
                 Log.w("WorldMapTask", "Invalid params; using [null, 0, 0]");
@@ -203,16 +252,83 @@ public class WorldMapView extends android.support.v7.widget.AppCompatImageView
             return makeBitmap(data, w, h);
         }
 
+        /**
+         * TODO: for better approach see https://gis.stackexchange.com/questions/17184/method-to-shade-or-overlay-a-raster-map-to-reflect-time-of-day-and-ambient-light
+         * @param data
+         * @param w
+         * @param h
+         * @return
+         */
         public Bitmap makeBitmap(SuntimesRiseSetDataset data, int w, int h)
         {
+            long bench_start = System.nanoTime();
             if (w <= 0 || h <= 0)
             {
                 return null;
             }
 
-            Bitmap b = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
-            //Canvas c = new Canvas(b);
-            //Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+            Bitmap b = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(b);
+            Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+            p.setColor(backgroundColor);                // draw background
+            c.drawRect(0, 0, w, h, p);
+
+            if (map != null)
+            {
+                Bitmap mapBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+                Canvas mapCanvas = new Canvas(mapBitmap);
+                map.setBounds(0, 0, mapCanvas.getWidth(), mapCanvas.getHeight());
+                map.draw(mapCanvas);
+                c.drawBitmap(mapBitmap, 0, 0, p);
+            }
+
+            if (data != null)
+            {
+                //Calendar now = data.now();
+                //TimeZone timeZone = now.getTimeZone();
+                //SuntimesCalculator calculator = data.calculator();
+
+                p.setColor(shadowColor);
+                for (int i=0; i<w; i++)                // draw shadow
+                {
+                    double lon = (((double)i / (double)w) * 360d) - 180d;  // i in [0,w] to [0,360] to [-180,180]
+                    for (int j=0; j<h; j++)
+                    {
+                        double lat = (((double)j / (double)h) * 180d) - 90d;      // j in [0,h] to [0,180] to [-90,90]
+                        // TODO
+                    }
+                }
+
+                // draw sun
+                int sunX = w/2;  // TODO
+                int sunY = h/2;  // TODO
+
+                p.setStyle(Paint.Style.FILL);
+                p.setColor(sunFillColor);
+                c.drawCircle(sunX, sunY, sunRadius, p);
+
+                p.setStyle(Paint.Style.STROKE);
+                p.setStrokeWidth(sunStroke);
+                p.setColor(sunStrokeColor);
+                c.drawCircle(sunX, sunY, sunRadius, p);
+
+                // draw moon
+                int moonX = w/4;  // TODO
+                int moonY = h/4;  // TODO
+
+                p.setStyle(Paint.Style.FILL);
+                p.setColor(moonFillColor);
+                c.drawCircle(moonX, moonY, moonRadius, p);
+
+                p.setStyle(Paint.Style.STROKE);
+                p.setStrokeWidth(moonStroke);
+                p.setColor(moonStrokeColor);
+                c.drawCircle(moonX, moonY, moonRadius, p);
+            }
+
+            long bench_end = System.nanoTime();
+            Log.d("DEBUG", "make world map :: " + ((bench_end - bench_start) / 1000000.0) + " ms");
             return b;
         }
 
