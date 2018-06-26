a3ed757fa034464333103665f83dff1d3c504080
Forrest Guice <forrestguice@users.noreply.github.com>
2018-06-26 08:54:21 -0700

    Update travis-debug-apk.sh

--- travis-debug-apk.sh
+++ travis-debug-apk.sh
@@ -7,7 +7,7 @@ git config --global user.email "forrestguice@gmail.com"
 git clone --quiet --branch gh-pages https://forrestguice:$GITHUB_API_KEY@github.com/forrestguice/SuntimesWidget gh-pages > /dev/null
 
 cd gh-pages/doc/debugapk
-cp -Rf $HOME/buildResults/app-debug.apk suntimes-debug-b$TRAVIS_BUILD_NUMBER.apk
+cp -Rf $HOME/buildResults/app-debug.apk suntimes-debug-lastbuild
 git add -f .
 
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
