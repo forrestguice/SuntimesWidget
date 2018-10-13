a4ff1c422d02d2cecbf70b7ea77fd2e75650e2cf
Forrest Guice <forrestguice@users.noreply.github.com>
2018-10-13 09:22:09 -0700

    spinner

--- app/src/main/java/com/forrestguice/suntimeswidget/lightmap/AnalemmaDialog.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/lightmap/AnalemmaDialog.java
@@ -32,11 +32,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.forrestguice.suntimeswidget.R;
 import com.forrestguice.suntimeswidget.SuntimesUtils;
 import com.forrestguice.suntimeswidget.calculator.SuntimesRiseSetDataset;
+import com.forrestguice.suntimeswidget.settings.WidgetSettings;
 import com.forrestguice.suntimeswidget.settings.WidgetTimezones;
 
 import java.util.Calendar;
@@ -49,13 +53,19 @@ public class AnalemmaDialog extends DialogFragment
     private static SuntimesUtils utils = new SuntimesUtils();
 
     private AnalemmaView analemma;
-    private TextView labelTime;
-    private TextView labelLocalMean;
-    private TextView labelApparentSolar;
-    private TextView labelEOT;
+    private TextView txtAnalemmaTime;
+    private Spinner spinnerMode;
+
+    private TextView txtLocalMean, txtApparentSolar, labelEOT;
+
+    private TextView txtEarliestSunrise, txtLatestSunrise;
+    private TextView txtEarliestSunset, txtLatestSunset;
+    private TextView txtShortestDay, txtLongestDay;
 
     private int decimalPlaces = 1;
     private View dialogContent = null;
+
+    private LightMapWidgetSettings.AnalemmaWidgetMode mode = LightMapWidgetSettings.PREF_DEF_APPEARANCE_WIDGETMODE_ANALEMMA;
 
     private SuntimesRiseSetDataset data;
     public void setData( SuntimesRiseSetDataset data )
@@ -134,33 +144,92 @@ public class AnalemmaDialog extends DialogFragment
         }
     };
 
+    /**
+     * analemmaTaskListener
+     */
+    private AnalemmaView.AnalemmaTaskListener analemmaTaskListener = new AnalemmaView.AnalemmaTaskListener()
+    {
+        @Override
+        public void onFinished(Bitmap result, AnalemmaView.AnalemmaData dataPoints)
+        {
+            super.onFinished(result, dataPoints);
+            if (txtAnalemmaTime != null && dataPoints != null)
+            {
+                txtAnalemmaTime.setText(utils.calendarDateTimeDisplayString(getContext(), dataPoints.getDate()).getValue());
+            }
+        }
+    };
+
+    /**
+     * onModeSelectedListener
+     */
+    private AdapterView.OnItemSelectedListener onModeSelectedListener = new AdapterView.OnItemSelectedListener()
+    {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+        {
+            LightMapWidgetSettings.AnalemmaWidgetMode selectedMode = (LightMapWidgetSettings.AnalemmaWidgetMode) parent.getItemAtPosition(position);
+            Context context = getContext();
+            if (context != null && mode != selectedMode)
+            {
+                mode = selectedMode;
+                analemma.getOptions().mode = mode;
+
+                LightMapWidgetSettings.saveAnalemmaModePref(context, 0, mode);
+                Log.d("Analemma", "onModeSelected: mode changed so triggering update...");
+                updateViews();
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {}
+    };
+
+    /**
+     * initViews
+     * @param dialogView
+     */
     public void initViews(View dialogView)
     {
-        analemma = (AnalemmaView)dialogView.findViewById(R.id.info_time_analemma);
-        AnalemmaView.AnalemmaOptions options = analemma.getOptions();
-        //options.showAxis = false;
-        options.mode = LightMapWidgetSettings.AnalemmaWidgetMode.DEC_EOT;
-        options.date_hour = 12;
-
-        analemma.setAnalemmaListener(new AnalemmaView.AnalemmaTaskListener()
+        Context context = getContext();
+        mode = LightMapWidgetSettings.loadAnalemmaModePref(context, 0);
+        spinnerMode = (Spinner)dialogView.findViewById(R.id.spinner_analemmaMode);
+        if (spinnerMode != null)
         {
-            @Override
-            public void onFinished(Bitmap result, AnalemmaView.AnalemmaDataPoints dataPoints)
-            {
-                super.onFinished(result, dataPoints);
-                if (labelTime != null && dataPoints != null)
-                {
-                    labelTime.setText(utils.calendarDateTimeDisplayString(getContext(), dataPoints.getDate()).getValue());
-                }
-            }
-        });
+            ArrayAdapter<LightMapWidgetSettings.AnalemmaWidgetMode> modeAdapter = new ArrayAdapter<>(context, R.layout.layout_listitem_oneline_alt, LightMapWidgetSettings.AnalemmaWidgetMode.values());
+            modeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            int modePosition = modeAdapter.getPosition(mode);
 
-        labelTime = (TextView)dialogView.findViewById(R.id.info_time_analemmaTime);
-        labelLocalMean = (TextView)dialogView.findViewById(R.id.info_time_localMean);
-        labelApparentSolar = (TextView)dialogView.findViewById(R.id.info_time_apparentSolar);
+            spinnerMode.setAdapter(modeAdapter);
+            spinnerMode.setOnItemSelectedListener(onModeSelectedListener);
+            spinnerMode.setSelection((modePosition >= 0) ? modePosition : 0);
+        }
+
+        analemma = (AnalemmaView)dialogView.findViewById(R.id.info_time_analemma);
+        if (analemma != null)
+        {
+            AnalemmaView.AnalemmaOptions options = analemma.getOptions();
+            options.mode = mode;
+            options.date_hour = 12;
+            analemma.setAnalemmaListener(analemmaTaskListener);
+        }
+
+        txtAnalemmaTime = (TextView)dialogView.findViewById(R.id.info_time_analemmaTime);
+        txtLocalMean = (TextView)dialogView.findViewById(R.id.info_time_localMean);
+        txtApparentSolar = (TextView)dialogView.findViewById(R.id.info_time_apparentSolar);
         labelEOT = (TextView)dialogView.findViewById(R.id.info_time_eotOffset);
+        txtEarliestSunrise = (TextView)dialogView.findViewById(R.id.info_time_sunrise_earliest);
+        txtLatestSunrise = (TextView)dialogView.findViewById(R.id.info_time_sunrise_latest);
+        txtEarliestSunset = (TextView)dialogView.findViewById(R.id.info_time_sunset_earliest);
+        txtLatestSunset = (TextView)dialogView.findViewById(R.id.info_time_sunset_latest);
+        txtShortestDay = (TextView)dialogView.findViewById(R.id.info_time_shorestday);
+        txtLongestDay = (TextView)dialogView.findViewById(R.id.info_time_longestday);
     }
 
+    /**
+     * themeViews
+     * @param context
+     */
     @SuppressWarnings("ResourceType")
     public void themeViews(Context context)
     {
@@ -186,6 +255,9 @@ public class AnalemmaDialog extends DialogFragment
         //colorLabel = field_night.label.getTextColors().getColorForState(new int[] { -android.R.attr.state_enabled }, Color.BLUE); // field_night.label.getCurrentTextColor()
     }
 
+    /**
+     * updateViews
+     */
     public void updateViews()
     {
         if (data != null)
@@ -201,6 +273,8 @@ public class AnalemmaDialog extends DialogFragment
 
     protected void updateAnalemmaViews(@NonNull SuntimesRiseSetDataset data)
     {
+        double longitude = data.location().getLongitudeAsDouble();
+
         if (analemma != null)
         {
             analemma.updateViews(data);
@@ -209,8 +283,59 @@ public class AnalemmaDialog extends DialogFragment
         if (labelEOT != null)
         {
             double eotOffset = WidgetTimezones.ApparentSolarTime.equationOfTimeOffset(data.calendar().get(Calendar.DAY_OF_YEAR));
-            labelEOT.setText( utils.timeDeltaLongDisplayString(0, (long)(eotOffset * 60 * 1000L), false, true, true).getValue() + " equation of time" ); // TODO
+            labelEOT.setText(utils.timeDeltaLongDisplayString(0, (long) (eotOffset * 60 * 1000L), false, true, true).getValue() + " EOT"); // TODO
         }
+
+        Context context = getContext();
+        if (context != null)
+        {
+            boolean showSeconds = WidgetSettings.loadShowSecondsPref(context, 0);
+
+            if (txtLocalMean != null)
+            {
+                Calendar now = Calendar.getInstance(new WidgetTimezones.LocalMeanTime(longitude, "Local Mean Time"));
+                SuntimesUtils.TimeDisplayText localMeanText = utils.calendarTimeShortDisplayString(context, now, showSeconds);
+                txtLocalMean.setText(localMeanText.toString() + " Local Mean");  // TODO
+            }
+
+            if (txtApparentSolar != null)
+            {
+                Calendar now = Calendar.getInstance(new WidgetTimezones.ApparentSolarTime(longitude, "Apparent Solar Time"));
+                SuntimesUtils.TimeDisplayText apparentSolarText = utils.calendarTimeShortDisplayString(context, now, showSeconds);
+                txtApparentSolar.setText(apparentSolarText.toString() + " Apparent Solar");  // TODO
+            }
+        }
+
+        if (txtEarliestSunrise != null)
+        {
+            txtEarliestSunrise.setText("TODO");
+        }
+
+        if (txtLatestSunrise != null)
+        {
+            txtLatestSunrise.setText("TODO");
+        }
+
+        if (txtEarliestSunset != null)
+        {
+            txtEarliestSunset.setText("TODO");
+        }
+
+        if (txtLatestSunset != null)
+        {
+            txtLatestSunset.setText("TODO");
+        }
+
+        if (txtShortestDay != null)
+        {
+            txtShortestDay.setText("TODO");
+        }
+
+        if (txtLongestDay != null)
+        {
+            txtLongestDay.setText("TODO");
+        }
+
     }
 
 }
