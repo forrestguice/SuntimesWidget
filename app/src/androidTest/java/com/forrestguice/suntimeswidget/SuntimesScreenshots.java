f3b8ec87bba36316504da96add911c0893b3c47a
Forrest Guice <forrestguice@users.noreply.github.com>
2018-10-13 11:58:33 -0700

    initScreenshots

--- app/src/androidTest/java/com/forrestguice/suntimeswidget/SuntimesScreenshots.java
+++ app/src/androidTest/java/com/forrestguice/suntimeswidget/SuntimesScreenshots.java
@@ -48,20 +48,21 @@ public class SuntimesScreenshots extends SuntimesActivityTestBase
     private static String version = BuildConfig.VERSION_NAME;
 
     private HashMap<String, ScreenshotConfig> config;
-    private ScreenshotConfig defaultConfig = new ScreenshotConfig(new WidgetSettings.Location("Iron Springs", "34.58742", "-112.57367"), "US/Arizona", false);
+    private ScreenshotConfig defaultConfig = new ScreenshotConfig(new WidgetSettings.Location("Phoenix", "33.45579", "-111.94580", "385"), "US/Arizona", false);
 
     @Before
     public void initScreenshots()
     {
         config = new HashMap<String, ScreenshotConfig>();
-        config.put("ca", new ScreenshotConfig(new WidgetSettings.Location("Barcelona", "41.3825", "2.1769"), "CET", true));
-        config.put("de", new ScreenshotConfig(new WidgetSettings.Location("Berlin", "52.5243", "13.4105"), "Europe/Berlin", true));
-        config.put("es_ES", new ScreenshotConfig(new WidgetSettings.Location("Madrid", "40.4378", "-3.8196"), "Europe/Madrid", true));
-        config.put("fr", new ScreenshotConfig(new WidgetSettings.Location("Paris", "48.8566", "2.3518"), "Europe/Paris", true));
-        config.put("hu", new ScreenshotConfig(new WidgetSettings.Location("Budapest", "47.4811", "18.9902"), "Europe/Budapest", true));
-        config.put("it", new ScreenshotConfig(new WidgetSettings.Location("Roma", "41.9099", "12.3959"), "CET", false));
-        config.put("pl", new ScreenshotConfig(new WidgetSettings.Location("Warszawa", "52.2319", "21.0067"), "Poland", true));
-        config.put("nb", new ScreenshotConfig(new WidgetSettings.Location("Oslo", "59.8937", "10.6450"), "Europe/Oslo", true));
+        config.put("ca", new ScreenshotConfig(new WidgetSettings.Location("Barcelona", "41.3825", "2.1769", "31"), "CET", true));
+        config.put("de", new ScreenshotConfig(new WidgetSettings.Location("Berlin", "52.5243", "13.4105", "40"), "Europe/Berlin", true));
+        config.put("es_ES", new ScreenshotConfig(new WidgetSettings.Location("Madrid", "40.4378", "-3.8196", "681"), "Europe/Madrid", true));
+        config.put("eu", new ScreenshotConfig(new WidgetSettings.Location("Euskal Herriko erdigunea", "42.883008", "-1.935491", "1258"), "CET", true));
+        config.put("fr", new ScreenshotConfig(new WidgetSettings.Location("Paris", "48.8566", "2.3518", "41"), "Europe/Paris", true));
+        config.put("hu", new ScreenshotConfig(new WidgetSettings.Location("Budapest", "47.4811", "18.9902", "225"), "Europe/Budapest", true));
+        config.put("it", new ScreenshotConfig(new WidgetSettings.Location("Roma", "41.9099", "12.3959", "79"), "CET", false));
+        config.put("pl", new ScreenshotConfig(new WidgetSettings.Location("Warszawa", "52.2319", "21.0067", "143"), "Poland", true));
+        config.put("nb", new ScreenshotConfig(new WidgetSettings.Location("Oslo", "59.8937", "10.6450", "0"), "Europe/Oslo", true));
 
         if (!version.startsWith("v"))
             version = "v" + version;
