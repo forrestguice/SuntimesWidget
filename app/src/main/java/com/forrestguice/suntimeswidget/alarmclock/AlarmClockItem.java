bbad5b628dac50ca6436a3c4cf60b45ea444ba25
Forrest Guice <forrestguice@users.noreply.github.com>
2018-10-31 16:06:15 -0700

    AlarmClockItem

--- app/src/main/java/com/forrestguice/suntimeswidget/alarmclock/AlarmClockItem.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/alarmclock/AlarmClockItem.java
@@ -19,13 +19,14 @@
 package com.forrestguice.suntimeswidget.alarmclock;
 
 import android.content.ContentValues;
+import android.util.Log;
 
 import com.forrestguice.suntimeswidget.settings.SolarEvents;
 import com.forrestguice.suntimeswidget.settings.WidgetSettings;
 
 public class AlarmClockItem
 {
-    protected int rowID = -1;
+    protected long rowID = -1;
     protected boolean enabled = false;
     protected boolean repeating = false;
     protected long timestamp = 0;
@@ -43,12 +44,32 @@ public class AlarmClockItem
 
     public AlarmClockItem(ContentValues alarm)
     {
-        rowID = alarm.getAsInteger(AlarmClockDatabaseAdapter.KEY_ROWID);
+        rowID = alarm.getAsLong(AlarmClockDatabaseAdapter.KEY_ROWID);
         enabled = (alarm.getAsInteger(AlarmClockDatabaseAdapter.KEY_ALARM_ENABLED) == 1);
         repeating = (alarm.getAsInteger(AlarmClockDatabaseAdapter.KEY_ALARM_REPEATING) == 1);
         timestamp = alarm.getAsLong(AlarmClockDatabaseAdapter.KEY_ALARM_DATETIME);
         label = alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_LABEL);
-        location = new WidgetSettings.Location(alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_PLACELABEL), alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_LATITUDE), alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_LONGITUDE), alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_ALTITUDE));
+
+        String locLat = alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_LATITUDE);
+        String locLon = alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_LONGITUDE);
+        if (locLat !=  null && locLon != null)
+        {
+            String locLabel = alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_PLACELABEL);
+            String locAlt = alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_ALTITUDE);
+            location = new WidgetSettings.Location(locLabel, locLat, locLon, locAlt);
+        } else location = null;
+
+        String eventString = alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_SOLAREVENT);
+        if (eventString != null)
+        {
+            try {
+                event = SolarEvents.valueOf(eventString);
+            } catch (IllegalArgumentException e) {
+                Log.w("AlarmClockItem", "Failed to load SolarEvent '" + eventString);
+                event = null;
+            }
+        }
+
         vibrate = (alarm.getAsInteger(AlarmClockDatabaseAdapter.KEY_ALARM_VIBRATE) == 1);
         ringtone = alarm.getAsString(AlarmClockDatabaseAdapter.KEY_ALARM_RINGTONE);
     }
@@ -62,6 +83,7 @@ public class AlarmClockItem
         values.put(AlarmClockDatabaseAdapter.KEY_ALARM_ENABLED, (enabled ? 1 : 0));
         values.put(AlarmClockDatabaseAdapter.KEY_ALARM_REPEATING, (repeating ? 1 : 0));
         values.put(AlarmClockDatabaseAdapter.KEY_ALARM_DATETIME, timestamp);
+        values.put(AlarmClockDatabaseAdapter.KEY_ALARM_DATETIME_OFFSET, offset);
         values.put(AlarmClockDatabaseAdapter.KEY_ALARM_LABEL, label);
 
         if (location != null)
@@ -70,6 +92,11 @@ public class AlarmClockItem
             values.put(AlarmClockDatabaseAdapter.KEY_ALARM_LATITUDE, location.getLatitude());
             values.put(AlarmClockDatabaseAdapter.KEY_ALARM_LONGITUDE, location.getLongitude());
             values.put(AlarmClockDatabaseAdapter.KEY_ALARM_ALTITUDE, location.getAltitude());
+        }
+
+        if (event != null)
+        {
+            values.put(AlarmClockDatabaseAdapter.KEY_ALARM_SOLAREVENT, event.name());
         }
 
         values.put(AlarmClockDatabaseAdapter.KEY_ALARM_VIBRATE, (vibrate ? 1 : 0));
