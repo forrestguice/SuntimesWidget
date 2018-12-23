af387a0058863683c2422a3787754b035be885cc
Forrest Guice <forrestguice@users.noreply.github.com>
2018-12-23 11:43:16 -0700

    widget
    implements  alt/az moon position widget

--- app/src/main/java/com/forrestguice/suntimeswidget/layouts/MoonLayout_1x1_5.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/layouts/MoonLayout_1x1_5.java
@@ -20,18 +20,17 @@ package com.forrestguice.suntimeswidget.layouts;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.text.SpannableString;
+import android.os.Build;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.RemoteViews;
 
 import com.forrestguice.suntimeswidget.R;
 import com.forrestguice.suntimeswidget.SuntimesUtils;
-import com.forrestguice.suntimeswidget.calculator.MoonPhaseDisplay;
 import com.forrestguice.suntimeswidget.calculator.SuntimesMoonData;
+import com.forrestguice.suntimeswidget.calculator.core.SuntimesCalculator;
 import com.forrestguice.suntimeswidget.settings.WidgetSettings;
 import com.forrestguice.suntimeswidget.themes.SuntimesTheme;
-
-import java.text.NumberFormat;
 
 public class MoonLayout_1x1_5 extends MoonLayout
 {
@@ -55,14 +54,44 @@ public class MoonLayout_1x1_5 extends MoonLayout
     public void updateViews(Context context, int appWidgetId, RemoteViews views, SuntimesMoonData data)
     {
         super.updateViews(context, appWidgetId, views, data);
+
+        SuntimesCalculator calculator = data.calculator();
+        SuntimesCalculator.MoonPosition moonPosition = (calculator != null ? calculator.getMoonPosition(data.now()) : null);
+        updateViewsAzimuthElevationText(context, views, moonPosition);
+
         boolean showLabels = WidgetSettings.loadShowLabelsPref(context, appWidgetId);
-        // TODO
+        int visibility = (showLabels ? View.VISIBLE : View.GONE);
+        views.setViewVisibility(R.id.info_moon_azimuth_current_label, visibility);
+        views.setViewVisibility(R.id.info_moon_elevation_current_label, visibility);
     }
+
+    protected int suffixColor = Color.GRAY;
+    protected int highlightColor = Color.WHITE;
 
     @Override
     public void themeViews(Context context, RemoteViews views, SuntimesTheme theme)
     {
         super.themeViews(context, views, theme);
+
+        highlightColor = theme.getTimeColor();
+        suffixColor = theme.getTimeSuffixColor();
+        int textColor = theme.getTextColor();
+
+        views.setTextColor(R.id.info_moon_azimuth_current_label, textColor);
+        views.setTextColor(R.id.info_moon_elevation_current_label, textColor);
+        views.setTextColor(R.id.info_moon_azimuth_current, textColor);
+        views.setTextColor(R.id.info_moon_elevation_current, textColor);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+        {
+            float textSize = theme.getTextSizeSp();
+            views.setTextViewTextSize(R.id.info_moon_azimuth_current_label, TypedValue.COMPLEX_UNIT_SP, textSize);
+            views.setTextViewTextSize(R.id.info_moon_elevation_current_label, TypedValue.COMPLEX_UNIT_SP, textSize);
+
+            float timeSize = theme.getTimeSizeSp();
+            views.setTextViewTextSize(R.id.info_moon_azimuth_current, TypedValue.COMPLEX_UNIT_SP, timeSize);
+            views.setTextViewTextSize(R.id.info_moon_elevation_current, TypedValue.COMPLEX_UNIT_SP, timeSize);
+        }
     }
 
     @SuppressWarnings("EmptyMethod")
@@ -71,4 +100,18 @@ public class MoonLayout_1x1_5 extends MoonLayout
     {
         // EMPTY
     }
+
+    protected void updateViewsAzimuthElevationText(Context context, RemoteViews views, SuntimesCalculator.MoonPosition moonPosition)
+    {
+        SuntimesUtils.TimeDisplayText azimuthDisplay = utils.formatAsDirection2(moonPosition.azimuth, PositionLayout.DECIMAL_PLACES, false);
+        views.setTextViewText(R.id.info_moon_azimuth_current, PositionLayout.styleAzimuthText(azimuthDisplay, highlightColor, suffixColor, boldTime));
+
+        if (Build.VERSION.SDK_INT >= 15) {
+            SuntimesUtils.TimeDisplayText azimuthDescription = utils.formatAsDirection2(moonPosition.azimuth, PositionLayout.DECIMAL_PLACES, true);
+            views.setContentDescription(R.id.info_moon_azimuth_current, utils.formatAsDirection(azimuthDescription.getValue(), azimuthDescription.getSuffix()));
+        }
+        views.setTextViewText(R.id.info_moon_elevation_current, PositionLayout.styleElevationText(moonPosition.elevation, highlightColor, suffixColor, boldTime));
+    }
+
+
 }
