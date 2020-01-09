b1174e8f93ca93257a01f148cb156a5a65d9976a
Forrest Guice <forrestguice@users.noreply.github.com>
2020-01-09 00:24:20 -0700

    ActionListActivity

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/actions/ActionListActivity.java
@@ -0,0 +1,177 @@
+/**
+    Copyright (C) 2019 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+package com.forrestguice.suntimeswidget.actions;
+
+import android.app.Activity;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import com.forrestguice.suntimeswidget.R;
+import com.forrestguice.suntimeswidget.calculator.SuntimesRiseSetData;
+import com.forrestguice.suntimeswidget.settings.AppSettings;
+import com.forrestguice.suntimeswidget.settings.WidgetSettings;
+
+public class ActionListActivity extends AppCompatActivity
+{
+    public static final int PICK_ACTION_REQUEST = 1;
+
+    public static final String SELECTED_ACTIONID = "actionID";
+    public static final String ADAPTER_MODIFIED = "isModified";
+    public static final String PARAM_SELECTED = "selected";
+    public static final String PARAM_NOSELECT = "noselect";
+
+    private ActionListHelper helper;
+    private boolean adapterModified = false;
+
+    private boolean disallowSelect = false;
+    private String preselectedAction;
+
+    public ActionListActivity() {
+        super();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase)
+    {
+        Context context = AppSettings.initLocale(newBase);
+        super.attachBaseContext(context);
+    }
+
+    @Override
+    public void onCreate(Bundle icicle)
+    {
+        setTheme(AppSettings.loadTheme(this));
+        super.onCreate(icicle);
+        WidgetSettings.initDefaults(this);
+        WidgetSettings.initDisplayStrings(this);
+
+        setResult(RESULT_CANCELED);
+        setContentView(R.layout.layout_activity_actionlist);
+
+        Intent intent = getIntent();
+        disallowSelect = intent.getBooleanExtra(PARAM_NOSELECT, disallowSelect);
+        preselectedAction = intent.getStringExtra(PARAM_SELECTED);
+
+        helper = new ActionListHelper(this, getSupportFragmentManager());
+        helper.initViews(this, findViewById(android.R.id.content), icicle);
+
+        initData(this);
+
+        Toolbar menuBar = (Toolbar) findViewById(R.id.app_menubar);
+        setSupportActionBar(menuBar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+        {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (preselectedAction != null && !preselectedAction.trim().isEmpty()) {
+            // TODO: select action
+        }
+    }
+
+    private View.OnClickListener onItemAccepted = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.putExtra(SELECTED_ACTIONID, helper.getIntentID());
+            intent.putExtra(ADAPTER_MODIFIED, adapterModified);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+            overridePendingTransition(R.anim.transition_ok_in, R.anim.transition_ok_out);
+        }
+    };
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        helper.setFragmentManager(getSupportFragmentManager());
+        helper.setOnItemAcceptedListener(onItemAccepted);
+        helper.onResume();
+    }
+
+    private SuntimesRiseSetData data;
+    private void initData(Context context)
+    {
+        data = new SuntimesRiseSetData(context, AppWidgetManager.INVALID_APPWIDGET_ID);   // use app configuration
+        data.setCompareMode(WidgetSettings.CompareMode.TOMORROW);
+        data.setTimeMode(WidgetSettings.TimeMode.OFFICIAL);
+        data.calculate();
+
+        SuntimesRiseSetData noonData = new SuntimesRiseSetData(data);
+        noonData.setTimeMode(WidgetSettings.TimeMode.NOON);
+        noonData.calculate();
+        data.linkData(noonData);
+    }
+
+    @Override
+    public void onBackPressed() {
+        onCancelled.onClick(null);
+    }
+    private View.OnClickListener onCancelled = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.putExtra(ADAPTER_MODIFIED, adapterModified);
+            setResult(((adapterModified) ? Activity.RESULT_OK : Activity.RESULT_CANCELED), intent);
+            finish();
+            overridePendingTransition(R.anim.transition_cancel_in, R.anim.transition_cancel_out);
+        }
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState( Bundle outState ) {
+        super.onSaveInstanceState(outState);
+        helper.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(@NonNull Bundle savedState) {
+        super.onRestoreInstanceState(savedState);
+        helper.onRestoreInstanceState(savedState);
+    }
+
+}
