bb7cb185ca5ed02c4e1148a1dabd1278cd3ed783
Forrest Guice <forrestguice@users.noreply.github.com>
2020-10-25 09:22:54 -0700

    isValid
    convenience methods

--- app/src/main/java/com/forrestguice/suntimeswidget/calculator/core/Location.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/calculator/core/Location.java
@@ -294,4 +294,12 @@ public class Location implements Parcelable
         }
     };
 
+    public static boolean isValidLatitude(double latitude) {
+        return (latitude >= -90 && latitude <= 90);
+    }
+
+    public static boolean isValidLongitude(double longitude) {
+        return (longitude >= -180 && longitude <= 180);
+    }
+
 }
