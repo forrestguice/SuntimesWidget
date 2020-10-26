56f7a52335913f3ad82c9dc541a4e18ebb79c1d1
Forrest Guice <forrestguice@users.noreply.github.com>
2020-10-26 11:10:12 -0700

    location extras
    Adds support for overriding setAlarm location with intent extras ("location_label", "latitude", "longitude", "altitude"). These extras are ignored if a LocationBundle ("location") is also included.

--- app/src/main/java/com/forrestguice/suntimeswidget/calculator/core/Location.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/calculator/core/Location.java
@@ -295,12 +295,4 @@ public class Location implements Parcelable
         }
     };
 
-    public static boolean isValidLatitude(double latitude) {
-        return (latitude >= -90 && latitude <= 90);
-    }
-
-    public static boolean isValidLongitude(double longitude) {
-        return (longitude >= -180 && longitude <= 180);
-    }
-
 }
