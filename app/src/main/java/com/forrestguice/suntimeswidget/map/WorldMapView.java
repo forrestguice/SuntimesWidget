8cfe9db22365349d33a2b3d8852dede516c076fb
Forrest Guice <forrestguice@users.noreply.github.com>
2022-01-28 11:31:22 -0700

    blue marble
    updates "Blue Marble" map background to include bathymetry.

--- app/src/main/java/com/forrestguice/suntimeswidget/map/WorldMapView.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/map/WorldMapView.java
@@ -142,7 +142,7 @@ public class WorldMapView extends android.support.v7.widget.AppCompatImageView
                 break;
 
             case EQUIRECTANGULAR_BLUEMARBLE:
-                options.map = ContextCompat.getDrawable(context, R.drawable.land_shallow_topo_1024);
+                options.map = ContextCompat.getDrawable(context, R.drawable.world_topo_bathy_1024x512);   // land_shallow_topo_1024);
                 options.map_night = ContextCompat.getDrawable(context, R.drawable.earth_lights_lrg_1024);
                 options.foregroundColor = Color.TRANSPARENT;
                 options.hasTransparentBaseMap = false;
