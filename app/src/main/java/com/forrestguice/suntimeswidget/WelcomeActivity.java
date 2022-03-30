1035a1a4673c8e83508e6a75fc022288e5ae7a31
Forrest Guice <forrestguice@users.noreply.github.com>
2022-03-30 08:48:14 -0700

    WelcomeActivity
    adds WelcomeActivity; first launch app configuration wizard

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/WelcomeActivity.java
@@ -0,0 +1,239 @@
+/**
+    Copyright (C) 2022 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.forrestguice.suntimeswidget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.forrestguice.suntimeswidget.calculator.core.Location;
+import com.forrestguice.suntimeswidget.settings.AppSettings;
+
+public class WelcomeActivity extends AppCompatActivity
+{
+    private int[] layouts = new int[] { R.layout.layout_welcome_app };  // TODO: welcome pages
+
+    private ViewPager pager;
+    private WelcomePagerAdapter pagerAdapter;
+    private Button nextButton, prevButton;
+    private LinearLayout indicatorLayout;
+    private AppSettings.LocaleInfo localeInfo;
+
+    @Override
+    protected void attachBaseContext(Context newBase)
+    {
+        Context context = AppSettings.initLocale(newBase, localeInfo = new AppSettings.LocaleInfo());
+        super.attachBaseContext(context);
+    }
+
+    /*@Override
+    public void onAttachFragment(Fragment fragment) {
+        super.onAttachFragment(fragment);
+    }*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        setTheme(AppSettings.loadTheme(this));
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= 21)
+        {
+            Window window = getWindow();
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        setContentView(R.layout.layout_activity_welcome);
+
+        pagerAdapter = new WelcomePagerAdapter();
+        pager = (ViewPager) findViewById(R.id.container);
+        pager.setAdapter(pagerAdapter);
+        pager.addOnPageChangeListener(pagerChangeListener);
+
+        prevButton = (Button) findViewById(R.id.button_prev);
+        if (prevButton != null) {
+            prevButton.setOnClickListener(onSkipPressed);
+        }
+
+        nextButton = (Button) findViewById(R.id.button_next);
+        if (nextButton != null) {
+            nextButton.setOnClickListener(onNextPressed);
+        }
+
+        indicatorLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+        pagerChangeListener.onPageSelected(0);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default: return (super.onOptionsItemSelected(menuItem));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        onDone();
+    }
+
+    private View.OnClickListener onSkipPressed = new View.OnClickListener() {
+        @Override
+        public void onClick(View v)
+        {
+            int position = pager.getCurrentItem();
+            if (position <= 0) {
+                onDone();
+            } else {
+                pager.setCurrentItem(position - 1);
+            }
+        }
+    };
+
+    private View.OnClickListener onNextPressed = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            int nextPosition = pager.getCurrentItem() + 1;
+            if (nextPosition < layouts.length) {
+                pager.setCurrentItem(nextPosition);
+            } else {
+                onDone();
+            }
+        }
+    };
+
+    private ViewPager.OnPageChangeListener pagerChangeListener = new ViewPager.OnPageChangeListener()
+    {
+        @Override
+        public void onPageSelected(int position)
+        {
+            setIndicator(position);
+            prevButton.setText(getString((position != 0) ? R.string.welcome_action_prev : R.string.welcome_action_skip));
+            nextButton.setText(getString((position != layouts.length-1) ? R.string.welcome_action_next : R.string.welcome_action_done));
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    };
+
+    @SuppressLint("ResourceType")
+    private void setIndicator(int position)
+    {
+        int[] colorAttrs = { R.attr.text_accentColor, R.attr.text_disabledColor };
+        TypedArray typedArray = obtainStyledAttributes(colorAttrs);
+        int activeColor = ContextCompat.getColor(WelcomeActivity.this, typedArray.getResourceId(0, R.color.text_accent_dark));
+        int inactiveColor = ContextCompat.getColor(WelcomeActivity.this, typedArray.getResourceId(1, R.color.text_disabled_dark));
+        typedArray.recycle();
+
+        TextView[] indicators = new TextView[layouts.length];
+        for (int i=0; i<indicators.length; i++)
+        {
+            indicators[i] = new TextView(this);
+            indicators[i].setTextSize(36);
+            indicators[i].setTextColor((i == position) ? activeColor : inactiveColor);
+            indicators[i].setText("\u2022");
+        }
+
+        indicatorLayout.removeAllViews();
+        for (TextView indicator : indicators) {
+            indicatorLayout.addView(indicator);
+        }
+    }
+
+    private void onDone() {
+        AppSettings.setFirstLaunch(WelcomeActivity.this, false);
+        finish();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * WelcomePagerAdapter
+     */
+    public class WelcomePagerAdapter extends PagerAdapter
+    {
+        public WelcomePagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position)
+        {
+            View v = null;
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            if (inflater != null) {
+                v = inflater.inflate(layouts[position], container, false);
+                container.addView(v);
+            }
+
+            if (v != null) {
+                TextView text0 = (TextView) v.findViewById(R.id.text0);
+                if (text0 != null) {
+                    text0.setText(SuntimesUtils.fromHtml(text0.getText().toString()));
+                }
+            }
+
+            return v;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+    }
+
+}
