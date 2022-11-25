forrestguice/SuntimesWidget

commit ef52c4894dc0bc9be26bfd118cf1a0ff5ca5d8b0

test

app/src/main/java/com/forrestguice/suntimeswidget/calculator/CalculatorProvider.java
@@ -164,9 +164,14 @@ public class CalculatorProvider extends ContentProvider
     private static final int URIMATCH_SEASONS_FOR_YEAR = 150;
     private static final int URIMATCH_SEASONS_FOR_RANGE = 160;
 
-    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-    static
+    private static UriMatcher uriMatcher;
+    private static UriMatcher uriMatcher()
     {
+        if (CalculatorProvider.uriMatcher != null) {
+            return uriMatcher();
+        }
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
         uriMatcher.addURI(AUTHORITY, QUERY_CONFIG, URIMATCH_CONFIG);
 
         uriMatcher.addURI(AUTHORITY, QUERY_SUN, URIMATCH_SUN);
@@ -190,6 +195,7 @@ public class CalculatorProvider extends ContentProvider
         uriMatcher.addURI(AUTHORITY, QUERY_SEASONS, URIMATCH_SEASONS);
         uriMatcher.addURI(AUTHORITY, QUERY_SEASONS + "/#", URIMATCH_SEASONS_FOR_YEAR);
         uriMatcher.addURI(AUTHORITY, QUERY_SEASONS + "/*", URIMATCH_SEASONS_FOR_RANGE);
+        return uriMatcher;
     }
 
     @Override
@@ -237,7 +243,7 @@ public class CalculatorProvider extends ContentProvider
         long[] range;
         Cursor retValue = null;
 
-        int uriMatch = uriMatcher.match(uri);
+        int uriMatch = uriMatcher().match(uri);
         switch (uriMatch)
         {
             case URIMATCH_CONFIG:

commit 60adaf3fabc58984b0cf9222ff30523f27cb6c61

CalculatorProvider
fixes bug introduced by ef52c4894dc0bc9be26bfd118cf1a0ff5ca5d8b0

app/src/main/java/com/forrestguice/suntimeswidget/calculator/CalculatorProvider.java
@@ -168,7 +168,7 @@ public class CalculatorProvider extends ContentProvider
     private static UriMatcher uriMatcher()
     {
         if (CalculatorProvider.uriMatcher != null) {
-            return uriMatcher();
+            return CalculatorProvider.uriMatcher;
         }
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 