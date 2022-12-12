5b4d9100a5937a2c41530fa9ec8aba1df8c1b1a4
Forrest Guice <forrestguice@users.noreply.github.com>
2022-12-11 19:36:48 -0700

    textSize
    adds "extra large" textsize

--- app/src/main/java/com/forrestguice/suntimeswidget/settings/AppSettings.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/settings/AppSettings.java
@@ -162,7 +162,10 @@ public class AppSettings
      */
     public static enum TextSize
     {
-        SMALL("Small"), NORMAL("Normal"), LARGE("Large");
+        SMALL("Small"),
+        NORMAL("Normal"),
+        LARGE("Large"),
+        XLARGE("Extra Large");
 
         private TextSize( String displayString ) {
             this.displayString = displayString;
@@ -181,6 +184,7 @@ public class AppSettings
             SMALL.setDisplayString(context.getString(R.string.textSize_small));
             NORMAL.setDisplayString(context.getString(R.string.textSize_normal));
             LARGE.setDisplayString(context.getString(R.string.textSize_large));
+            XLARGE.setDisplayString(context.getString(R.string.textSize_xlarge));
         }
 
         public static TextSize valueOf(String value, TextSize defaultValue)
@@ -915,6 +919,7 @@ public class AppSettings
             switch (size) {
                 case SMALL: return R.style.AppTheme_System_Small;
                 case LARGE: return R.style.AppTheme_System_Large;
+                case XLARGE: return R.style.AppTheme_System_XLarge;
                 case NORMAL: default: return R.style.AppTheme_System;
             }
         }
@@ -935,6 +940,7 @@ public class AppSettings
             switch (size) {
                 case SMALL: return R.style.AppTheme_Light_Small;
                 case LARGE: return R.style.AppTheme_Light_Large;
+                case XLARGE: return R.style.AppTheme_Light_XLarge;
                 case NORMAL: default: return R.style.AppTheme_Light;
             }
         }
@@ -954,6 +960,7 @@ public class AppSettings
             switch (size) {
                 case SMALL: return R.style.AppTheme_Dark_Small;
                 case LARGE: return R.style.AppTheme_Dark_Large;
+                case XLARGE: return R.style.AppTheme_Dark_XLarge;
                 case NORMAL: default: return R.style.AppTheme_Dark;
             }
         }
