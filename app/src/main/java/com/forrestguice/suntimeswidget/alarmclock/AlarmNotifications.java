c7449ebbcce34d42adbc9cdeb7eb4fc26d6d3099
Forrest Guice <forrestguice@users.noreply.github.com>
2022-12-18 02:36:34 -0700

    AlarmNotifications
    dismissNotification

--- app/src/main/java/com/forrestguice/suntimeswidget/alarmclock/AlarmNotifications.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/alarmclock/AlarmNotifications.java
@@ -1910,6 +1910,7 @@ public class AlarmNotifications extends BroadcastReceiver
                         addAlarmTimeout(context, ACTION_SHOW, item.getUri(), item.alarmtime);
                         context.sendBroadcast(getFullscreenBroadcast(item.getUri()));
                     }
+                    notifications.dismissNotification(context, (int)item.rowID);
                     if (chained != null) {
                         chained.onFinished(true, item);
                     } else notifications.stopSelf(startId);
