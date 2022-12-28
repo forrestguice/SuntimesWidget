7486f4733ba52c63f58e8f0dd2f173abc9628348
Forrest Guice <forrestguice@users.noreply.github.com>
2022-12-28 09:19:55 -0700

    icon
    fix about icon

--- app/src/main/java/com/forrestguice/suntimeswidget/alarmclock/ui/AlarmEditActivity.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/alarmclock/ui/AlarmEditActivity.java
@@ -606,7 +606,7 @@ public class AlarmEditActivity extends AppCompatActivity implements AlarmItemAda
     protected void showAbout()
     {
         Intent about = new Intent(this, AboutActivity.class);
-        about.putExtra(AboutActivity.EXTRA_ICONID, R.mipmap.ic_launcher_alarms_round);
+        about.putExtra(AboutActivity.EXTRA_ICONID, R.drawable.ic_suntimesalarms);
         startActivity(about);
         overridePendingTransition(R.anim.transition_next_in, R.anim.transition_next_out);
     }
