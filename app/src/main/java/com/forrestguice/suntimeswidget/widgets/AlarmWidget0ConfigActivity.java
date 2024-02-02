32605066fd2787e839b561a30e4f0c6e4ce12493
Forrest Guice <forrestguice@users.noreply.github.com>
2024-02-02 10:56:34 -0700

    alarm widget
    adds alarm widget (stubbed) (#766)

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/widgets/AlarmWidget0ConfigActivity.java
@@ -0,0 +1,110 @@
+/**
+    Copyright (C) 2024 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
+
+package com.forrestguice.suntimeswidget.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Spinner;
+
+import com.forrestguice.suntimeswidget.R;
+import com.forrestguice.suntimeswidget.SuntimesConfigActivity0;
+import com.forrestguice.suntimeswidget.calculator.SuntimesCalculatorDescriptor;
+import com.forrestguice.suntimeswidget.themes.WidgetThemeConfigActivity;
+
+/**
+ * Alarm widget config activity.
+ */
+@SuppressWarnings("Convert2Diamond")
+public class AlarmWidget0ConfigActivity extends SuntimesConfigActivity0
+{
+    public AlarmWidget0ConfigActivity() {
+        super();
+    }
+
+    @Override
+    protected Class getWidgetClass() {
+        return AlarmWidget0.class;
+    }
+
+    @Override
+    protected void initViews( Context context )
+    {
+        super.initViews(context);
+        setConfigActivityTitle(getString(R.string.app_name_alarmwidget0));
+
+        // TODO: options
+        showCalendarMode(true);
+        showCalendarFormat(true);
+        showOptionLabels(true);
+
+        showOptionShowDate(false);    // always true
+        showTimeFormatMode(false);
+        showOptionRiseSetOrder(false);
+        hideOptionUseAltitude();
+        hideOptionCompareAgainst();
+        hideOption1x1LayoutMode();
+        showOptionWeeks(false);
+        showOptionHours(false);
+        showOptionTimeDate(false);
+        hideOptionShowSeconds();
+        showOptionTrackingMode(false);
+        showOptionTimeModeOverride(false);
+        showDataSource(false);
+        showTimeMode(false);
+        showOptionShowNoon(false);
+
+        hideLayoutSettings();
+
+        moveSectionToTop(R.id.appwidget_general_layout);
+    }
+
+    @Override
+    protected void updateWidgets(Context context, int[] appWidgetIds)
+    {
+        Intent updateIntent = new Intent(context, getWidgetClass());
+        updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+        sendBroadcast(updateIntent);
+    }
+
+    @Override
+    protected int getAboutIconID() {
+        return R.drawable.ic_action_alarms;
+    }
+
+    @Override
+    protected SuntimesCalculatorDescriptor[] supportingCalculators() {
+        return SuntimesCalculatorDescriptor.values(this, requiredFeatures);
+    }
+    private static int[] requiredFeatures = new int[] {};
+
+    @Override
+    protected boolean getDefaultScaleText() {
+        return true;
+    }
+
+    @Override
+    protected Intent themeEditorIntent(Context context)
+    {
+        Intent intent = super.themeEditorIntent(context);
+        intent.putExtra(WidgetThemeConfigActivity.PARAM_PREVIEWID, WidgetThemeConfigActivity.PREVIEWID_CLOCK_1x1);    // TODO: previewID
+        return intent;
+    }
+}
