9385758b5a04572f77a0997356a2e0263fbe7fac
Forrest Guice <forrestguice@users.noreply.github.com>
2024-06-14 13:33:32 -0700

    AppColorKeys

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/colors/AppColorKeys.java
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+    Copyright (C) 2024 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.forrestguice.suntimeswidget.colors;
+
+public class AppColorKeys
+{
+    public static final String COLOR_DAY = "color_day";
+    public static final String COLOR_NIGHT = "color_night";
+    public static final String COLOR_CIVIL = "color_civil";
+    public static final String COLOR_NAUTICAL = "color_nautical";
+    public static final String COLOR_ASTRONOMICAL = "color_astronomical";
+
+    public static final String COLOR_RISING = "color_rising";
+    public static final String COLOR_RISING_TEXT = "color_risingText";
+    public static final String COLOR_SETTING = "color_setting";
+    public static final String COLOR_SETTING_TEXT = "color_settingText";
+
+    public static final String COLOR_SUN_FILL = "color_sunFill";
+    public static final String COLOR_SUN_STROKE = "color_sunStroke";
+
+    public static final String COLOR_MOON_FILL = "color_moonFill";
+    public static final String COLOR_MOON_STROKE = "color_moonStroke";
+
+    public static final String COLOR_POINT_FILL = "color_pointFill";
+    public static final String COLOR_POINT_STROKE = "color_pointStroke";
+
+    public static final String COLOR_AXIS = "color_axis";
+    public static final String COLOR_GRID_MAJOR = "color_grid_major";
+    public static final String COLOR_GRID_MINOR = "color_grid_minor";
+    public static final String COLOR_LABELS = "color_labels";
+    public static final String COLOR_LABELS_BG = "color_labels_bg";
+
+    public static final String COLOR_SPRING = "color_spring";
+    public static final String COLOR_SPRING_TEXT = "color_springText";
+    public static final String COLOR_SUMMER = "color_summer";
+    public static final String COLOR_SUMMER_TEXT = "color_summerText";
+    public static final String COLOR_AUTUMN = "color_autumn";
+    public static final String COLOR_AUTUMN_TEXT = "color_autumnText";
+    public static final String COLOR_WINTER = "color_winter";
+    public static final String COLOR_WINTER_TEXT = "color_winterText";
+
+    public static final String COLOR_SUNPATH_DAY_STROKE = "color_sunpath_day_stroke";
+    public static final String COLOR_SUNPATH_DAY_FILL = "color_sunpath_day_fill";
+    public static final String COLOR_SUNPATH_NIGHT_STROKE = "color_sunpath_night_stroke";
+    public static final String COLOR_SUNPATH_NIGHT_FILL = "color_sunpath_night_fill";
+
+    public static final String COLOR_MOONPATH_DAY_STROKE = "color_moonpath_day_stroke";
+    public static final String COLOR_MOONPATH_DAY_FILL = "color_moonpath_day_fill";
+    public static final String COLOR_MOONPATH_NIGHT_STROKE = "color_moonpath_night_stroke";
+    public static final String COLOR_MOONPATH_NIGHT_FILL = "color_moonpath_night_fill";
+
+}
