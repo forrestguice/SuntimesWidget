1c3772a9f4a94e5dafe6ebd3dc5ce6ff0190e869
Forrest Guice <forrestguice@users.noreply.github.com>
2025-01-03 20:36:32 -0700

    ClockLayout_1x1_1
    clock typeface settings

--- app/src/main/java/com/forrestguice/suntimeswidget/widgets/layouts/ClockLayout_1x1_1.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/widgets/layouts/ClockLayout_1x1_1.java
@@ -21,15 +21,19 @@ package com.forrestguice.suntimeswidget.widgets.layouts;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.util.TypedValue;
 import android.widget.RemoteViews;
 
 import com.forrestguice.suntimeswidget.R;
 import com.forrestguice.suntimeswidget.SuntimesUtils;
+import com.forrestguice.suntimeswidget.settings.AppSettings;
 import com.forrestguice.suntimeswidget.settings.WidgetSettings;
+import com.forrestguice.suntimeswidget.widgets.ClockWidgetSettings;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -61,6 +65,18 @@ public class ClockLayout_1x1_1 extends ClockLayout_1x1_0
         return 128;
     }
 
+    protected ClockFaceOptions initClockFaceOptions(Context context, int appWidgetId)
+    {
+        if (options == null) {
+            options = new ClockFaceOptions(context, appWidgetId);
+        }
+        options.textColor = ClockWidgetSettings.loadClockTypefaceValue(context, appWidgetId, ClockWidgetSettings.PREF_KEY_APPEARANCE_TYPEFACE_COLOR, timeColor);
+        options.minTextSizePx = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, textSizeSp, context.getResources().getDisplayMetrics());
+        options.textAlign = AppSettings.isLocaleRtl(context) ? Paint.Align.LEFT : Paint.Align.RIGHT;
+        return options;
+    }
+    protected ClockFaceOptions options = null;
+
     @Override
     protected void updateTimeViews(Context context, int appWidgetId, RemoteViews views, Calendar now)
     {
@@ -75,98 +91,10 @@ public class ClockLayout_1x1_1 extends ClockLayout_1x1_0
         //w = dpWidth;
         //h = dpHeight;
 
-        Bitmap b = makeClockBitmap(context, w, h, nowString, "monospace", false, false, true, timeColor);
+        ClockFaceOptions o = initClockFaceOptions(context, appWidgetId);
+        Bitmap b = new ClockFaceBitmap().makeClockBitmap(context, w, h, nowString, o);
         views.setImageViewBitmap(R.id.image_time, b);
         views.setContentDescription(R.id.image_time, nowString);
-    }
-
-    protected void initPaint(Context context)
-    {
-        if (pDebug == null)
-        {
-            pDebug = new Paint();
-            pDebug.setStyle(Paint.Style.STROKE);
-            pDebug.setStrokeWidth(1);    // debug circles
-        }
-
-        if (p == null) {
-            p = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        }
-    }
-    private Paint pDebug;
-    private TextPaint p;
-
-    protected static StaticLayout getStaticLayout(String text, String[] lines, TextPaint p)
-    {
-        return StaticLayout.Builder.obtain(text, 0, text.length(), p, (int) getWidth(lines, p))
-            .setLineSpacing(0, 0.90f).build();
-    }
-
-    protected static float getWidth(String[] lines, TextPaint p)
-    {
-        float width = 0;
-        for (int i=0; i<lines.length; i++)
-        {
-            float s = p.measureText(lines[i]);
-            if (s > width) {
-                width = s;
-            }
-        }
-        return width;
-    }
-
-    protected Bitmap makeClockBitmap(Context context, int w, int h, String text, String fontFamily, boolean bold, boolean italic, boolean outline, int textColor)
-    {
-        initPaint(context);
-        Bitmap b = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(b);
-
-        //pDebug.setStyle(Paint.Style.STROKE);
-        //pDebug.setStrokeWidth(1);    // debug circles
-        //c.drawColor(Color.LTGRAY);
-        //c.drawCircle(w/2f, h/2f, w/4f, pDebug);
-        //c.drawCircle(w/2f, h/2f, w/2f, pDebug);
-
-        p.setColor(textColor);
-        p.setTypeface(Typeface.create(fontFamily,
-                bold ? italic ? Typeface.BOLD_ITALIC : Typeface.BOLD
-                        : italic ? Typeface.ITALIC : Typeface.NORMAL));
-
-        float textSizePx = 16;
-        p.setTextSize(textSizePx);
-
-        String[] lines = text.split("\n");
-        StaticLayout layout = getStaticLayout(text, lines, p);
-        while (layout.getWidth() < w && layout.getHeight() < h)
-        {
-            textSizePx += 2;
-            p.setTextSize(textSizePx);
-            layout = getStaticLayout(text, lines, p);
-        }
-        while (layout.getWidth() > w || layout.getHeight() > h)
-        {
-            textSizePx -= 1;
-            p.setTextSize(textSizePx);
-            layout = getStaticLayout(text, lines, p);
-        }
-
-        p.setStyle(outline ? Paint.Style.STROKE : Paint.Style.FILL);
-        if (outline)
-        {
-            p.setStrokeWidth(textSizePx / 32);
-            p.setStrokeJoin(Paint.Join.ROUND);
-            p.setStrokeMiter(1);
-
-            //float shadowWidthPx = textStrokePx / 2;
-            //float shadowOffsetX = textStrokePx;
-            //float shadowOffsetY = textStrokePx;
-            //p.setShadowLayer(shadowWidthPx, shadowOffsetX, shadowOffsetY, Color.BLACK);
-        }
-
-        p.setTextAlign(Paint.Align.RIGHT);
-        c.translate(w/2f + (layout.getWidth() / 2f), 0);
-        layout.draw(c);
-        return b;
     }
 
     protected boolean is24(Context context, int appWidgetId)
@@ -184,6 +112,152 @@ public class ClockLayout_1x1_1 extends ClockLayout_1x1_0
     protected SimpleDateFormat hourFormat12 = new SimpleDateFormat("h", SuntimesUtils.getLocale());
     protected SimpleDateFormat hourFormat24 = new SimpleDateFormat("HH", SuntimesUtils.getLocale());
     protected SimpleDateFormat minuteFormat = new SimpleDateFormat("mm", SuntimesUtils.getLocale());
+
+    /**
+     * ClockFaceBitmap
+     */
+    public static class ClockFaceBitmap
+    {
+        protected void initPaint(Context context)
+        {
+            if (pDebug == null)
+            {
+                pDebug = new Paint();
+                pDebug.setStyle(Paint.Style.STROKE);
+                pDebug.setStrokeWidth(1);    // debug circles
+            }
+
+            if (p == null) {
+                p = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+            }
+        }
+        private Paint pDebug;
+        private TextPaint p;
+
+        protected Bitmap makeClockBitmap(Context context, int w, int h, String text, ClockFaceOptions options)
+        {
+            initPaint(context);
+            Bitmap b = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(b);
+
+            //c.drawColor(Color.LTGRAY);
+            //c.drawCircle(w/2f, h/2f, w/4f, pDebug);
+            //c.drawCircle(w/2f, h/2f, w/2f, pDebug);
+
+            p.setColor(options.textColor);
+            p.setTypeface(options.getTypeface());
+
+            float textSizePx = options.minTextSizePx;
+            p.setTextSize(textSizePx);
+
+            String[] lines = text.split("\n");
+            StaticLayout layout = getStaticLayout(text, lines, p);
+
+            if (options.scaleText)
+            {
+                while (layout.getWidth() < w && layout.getHeight() < h)
+                {
+                    textSizePx += 2;
+                    p.setTextSize(textSizePx);
+                    layout = getStaticLayout(text, lines, p);
+                }
+                while (layout.getWidth() > w || layout.getHeight() > h)
+                {
+                    textSizePx -= 1;
+                    p.setTextSize(textSizePx);
+                    layout = getStaticLayout(text, lines, p);
+                }
+            }
+
+            p.setStyle(options.outline ? Paint.Style.STROKE : Paint.Style.FILL);
+            if (options.outline)
+            {
+                p.setStrokeWidth(textSizePx * options.outlineRatio);
+                p.setStrokeJoin(Paint.Join.ROUND);
+                //p.setStrokeMiter(10);
+                p.setStrokeCap(Paint.Cap.ROUND);
+            }
+
+            if (options.glow) {
+                p.setShadowLayer(textSizePx * options.glowRatio, options.glowX, options.glowY, options.glowColor);
+            } else {
+                p.clearShadowLayer();
+            }
+
+            p.setTextAlign(options.textAlign);
+            switch (options.textAlign)
+            {
+                case CENTER: c.translate(w/2f, 0); break;
+                case LEFT: c.translate(w/2f - (layout.getWidth() / 2f), 0); break;
+                case RIGHT: default: c.translate(w/2f + (layout.getWidth() / 2f), 0); break;
+            }
+            layout.draw(c);
+            return b;
+        }
+
+        protected static StaticLayout getStaticLayout(String text, String[] lines, TextPaint p)
+        {
+            return StaticLayout.Builder.obtain(text, 0, text.length(), p, (int) getWidth(lines, p))
+                    .setLineSpacing(0, 0.90f).build();
+        }
+
+        protected static float getWidth(String[] lines, TextPaint p)
+        {
+            float width = 0;
+            for (int i=0; i<lines.length; i++)
+            {
+                float s = p.measureText(lines[i]);
+                if (s > width) {
+                    width = s;
+                }
+            }
+            return width;
+        }
+    }
+
+    /**
+     * ClockFaceOptions
+     */
+    public static class ClockFaceOptions
+    {
+        public int textColor = Color.WHITE;
+        public Paint.Align textAlign = Paint.Align.RIGHT;
+
+        public String fontFamily = "sans-serif-black";    // casual, cursive, monospace, sans-serif, sans-serif-black, sans-serif-condensed, sans-serif-condensed-light, sans-serif-light, sans-serif-medium, sans-serif-thin, serif, serif-monospace
+        public boolean bold = false;
+        public boolean italic = false;
+
+        public boolean outline = true;
+        public float outlineRatio = 1/48f;
+
+        public boolean glow = false;
+        public float glowRatio = 3/48f;
+        public int glowX = 1, glowY = 1;
+        public int glowColor = Color.WHITE;
+
+        public boolean scaleText = true;
+        public float minTextSizePx = 16;
+
+        public ClockFaceOptions(Context context, int appWidgetId)
+        {
+            scaleText = WidgetSettings.loadScaleTextPref(context, appWidgetId, scaleText);
+            textColor = ClockWidgetSettings.loadClockTypefaceValue(context, appWidgetId, ClockWidgetSettings.PREF_KEY_APPEARANCE_TYPEFACE_COLOR, textColor);
+            fontFamily = ClockWidgetSettings.loadClockTypefacePref(context, appWidgetId, fontFamily);
+            bold = ClockWidgetSettings.loadClockTypefaceFlag(context, appWidgetId, ClockWidgetSettings.PREF_KEY_APPEARANCE_TYPEFACE_BOLD, bold);
+            italic = ClockWidgetSettings.loadClockTypefaceFlag(context, appWidgetId, ClockWidgetSettings.PREF_KEY_APPEARANCE_TYPEFACE_ITALIC, italic);
+            outline = ClockWidgetSettings.loadClockTypefaceFlag(context, appWidgetId, ClockWidgetSettings.PREF_KEY_APPEARANCE_TYPEFACE_OUTLINE, outline);
+        }
+
+        public Typeface getTypeface() {
+            return getTypeface(fontFamily, bold, italic);
+        }
+        public static Typeface getTypeface(String fontFamily, boolean bold, boolean italic)
+        {
+            return Typeface.create(fontFamily,
+                    bold ? italic ? Typeface.BOLD_ITALIC : Typeface.BOLD
+                            : italic ? Typeface.ITALIC : Typeface.NORMAL);
+        }
+    }
 
     /*
     // this variation uses a TextView and LinearLayout to arrange text onto the canvas
