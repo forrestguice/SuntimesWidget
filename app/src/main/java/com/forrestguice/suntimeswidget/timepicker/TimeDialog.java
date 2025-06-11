892b0ff348aac83b20c5a7375b0e70113ad44812
Forrest Guice <forrestguice@users.noreply.github.com>
2025-06-11 11:02:14 -0700

    DateDialog
    adds pick date bottom sheet dialog

---
+++ app/src/main/java/com/forrestguice/suntimeswidget/timepicker/TimeDialog.java
@@ -0,0 +1,157 @@
+/**
+    Copyright (C) 2025 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package com.forrestguice.suntimeswidget.timepicker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.TimePicker;
+
+import com.forrestguice.suntimeswidget.R;
+
+/**
+ * A "time selection" bottom sheet dialog.
+ */
+public class TimeDialog extends TimeDialogBase
+{
+    public static final String KEY_DIALOG_HOUR = "dialog_hour";
+    public static final String KEY_DIALOG_MINUTE = "dialog_minute";
+
+    protected TimePicker timePicker;
+
+    public TimeDialog() {
+        super();
+    }
+
+    @Override
+    protected int getDialogLayoutResID() {
+        return R.layout.layout_dialog_time;
+    }
+    protected int getTimePickerResID() {
+        return R.id.pick_time;
+    }
+
+    protected void initViews(Context context, View dialogContent)
+    {
+        super.initViews(context, dialogContent);
+        timePicker = (TimePicker) dialogContent.findViewById(getTimePickerResID());
+        if (timePicker != null)
+        {
+            timePicker.setHour(getInitialHour());
+            timePicker.setMinute(getInitialMinute());
+            timePicker.setIs24HourView(timeIs24());
+        }
+    }
+
+    @Override
+    public void loadSettings(@Nullable Activity activity)
+    {
+        if (activity != null)
+        {
+            SharedPreferences prefs = activity.getPreferences(Context.MODE_PRIVATE);
+            int hour = prefs.getInt(KEY_DIALOG_HOUR, -1);
+            int minute = prefs.getInt(KEY_DIALOG_MINUTE, -1);
+
+            if (hour != -1) {
+                getArgs().putInt(KEY_DIALOG_HOUR, hour);
+            }
+            if (minute != -1) {
+                getArgs().putInt(KEY_DIALOG_MINUTE, minute);
+            }
+
+            if (timePicker != null && hour != -1 && minute != -1) {
+                timePicker.setHour(hour);
+                timePicker.setHour(minute);
+            }
+        }
+    }
+
+    @Override
+    public void saveSettings(@Nullable Activity activity)
+    {
+        if (activity != null) {
+            SharedPreferences.Editor prefs = activity.getPreferences(Context.MODE_PRIVATE).edit();
+            prefs.putInt(KEY_DIALOG_HOUR, getSelectedHour());
+            prefs.putInt(KEY_DIALOG_MINUTE, getSelectedMinute());
+            prefs.apply();
+        }
+    }
+
+    public int getSelectedHour() {
+        return (timePicker != null ? timePicker.getHour() : getInitialHour());
+    }
+
+    public int getSelectedMinute() {
+        return (timePicker != null ? timePicker.getMinute() : getInitialMinute());
+    }
+
+    public void setInitialTime(String hour, String minute) {
+        try {
+            setInitialTime(Integer.parseInt(hour), Integer.parseInt(minute));
+        } catch (NumberFormatException e) {
+            Log.e("TimeDialog", "setInitialTime: invalid input; " + e);
+        }
+    }
+    public void setInitialTime(int hour, int minute) {
+        getArgs().putInt(KEY_DIALOG_HOUR, hour);
+        getArgs().putInt(KEY_DIALOG_MINUTE, minute);
+    }
+    public int getInitialHour() {
+        return getArgs().getInt(KEY_DIALOG_HOUR, 12);
+    }
+    public int getInitialMinute() {
+        return getArgs().getInt(KEY_DIALOG_MINUTE, 0);
+    }
+
+    @Override
+    public TimeDialogResult getSelected()
+    {
+        return new TimeDialogResult()
+        {
+            @Override
+            public Integer getYear() {
+                return null;
+            }
+            @Override
+            public Integer getMonth() {
+                return null;
+            }
+            @Override
+            public Integer getDay() {
+                return null;
+            }
+
+            @Override
+            public Integer getHour() {
+                return getSelectedHour();
+            }
+            @Override
+            public Integer getMinute() {
+                return getSelectedMinute();
+            }
+            @Override
+            public Integer getSecond() {
+                return 0;
+            }
+        };
+    }
+
+}
--- app/src/main/java/com/forrestguice/suntimeswidget/timepicker/TimeDialogBase.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/timepicker/TimeDialogBase.java
@@ -15,8 +15,9 @@
     You should have received a copy of the GNU General Public License
     along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
 */
-package com.forrestguice.suntimeswidget;
+package com.forrestguice.suntimeswidget.timepicker;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -26,7 +27,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.BottomSheetDialogFragment;
-import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,32 +35,31 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
+import com.forrestguice.suntimeswidget.R;
 import com.forrestguice.suntimeswidget.settings.AppSettings;
 import com.forrestguice.suntimeswidget.views.TooltipCompat;
 import com.forrestguice.suntimeswidget.views.ViewUtils;
 
 @SuppressWarnings("Convert2Diamond")
-public class TimeDialog extends BottomSheetDialogFragment
+public abstract class TimeDialogBase extends BottomSheetDialogFragment
 {
     public static final String KEY_DIALOG_TITLE = "dialog_title";
-    public static final String KEY_DIALOG_HOUR = "dialog_hour";
-    public static final String KEY_DIALOG_MINUTE = "dialog_minute";
     public static final String KEY_DIALOG_IS24 = "dialog_is24";
 
-    protected TimePicker picker;
     protected ImageButton acceptButton;
 
-    public TimeDialog() {
+    public TimeDialogBase() {
         setArguments(new Bundle());
     }
+
+    protected abstract int getDialogLayoutResID();
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedState)
     {
         ContextThemeWrapper contextWrapper = new ContextThemeWrapper(getActivity(), AppSettings.loadTheme(getContext()));    // hack: contextWrapper required because base theme is not properly applied
-        View dialogContent = inflater.cloneInContext(contextWrapper).inflate(R.layout.layout_dialog_time, parent, false);
+        View dialogContent = inflater.cloneInContext(contextWrapper).inflate(getDialogLayoutResID(), parent, false);
         initViews(getContext(), dialogContent);
         if (savedState != null) {
             loadSettings(savedState);
@@ -70,19 +69,11 @@ public class TimeDialog extends BottomSheetDialogFragment
 
     protected void initViews(Context context, View dialogContent)
     {
-        picker = (TimePicker) dialogContent.findViewById(R.id.pick_time);
-        if (picker != null)
-        {
-            picker.setHour(getInitialHour());
-            picker.setMinute(getInitialMinute());
-            picker.setIs24HourView(timeIs24());
-        }
-
         ImageButton cancelButton = (ImageButton) dialogContent.findViewById(R.id.dialog_button_cancel);
         if (cancelButton != null) {
             TooltipCompat.setTooltipText(cancelButton, cancelButton.getContentDescription());
             cancelButton.setOnClickListener(onDialogCancelClick);
-            if (AppSettings.isTelevision(getActivity())) {
+            if (AppSettings.isTelevision(context)) {
                 cancelButton.setFocusableInTouchMode(true);
             }
         }
@@ -94,8 +85,14 @@ public class TimeDialog extends BottomSheetDialogFragment
         }
 
         Button neutralButton = (Button) dialogContent.findViewById(R.id.dialog_button_neutral);
-        if (neutralButton != null) {
-            neutralButton.setOnClickListener(onDialogNeutralClick);
+        if (neutralButton != null)
+        {
+            if (getNeutralButtonLabel() != null) {
+                neutralButton.setText(getNeutralButtonLabel());
+                neutralButton.setOnClickListener(onDialogNeutralClick);
+            } else {
+                neutralButton.setVisibility(View.GONE);
+            }
         }
 
         String title = getDialogTitle();
@@ -103,6 +100,11 @@ public class TimeDialog extends BottomSheetDialogFragment
         if (text_title != null && title != null) {
             text_title.setText(title);
         }
+    }
+
+    @Nullable
+    protected CharSequence getNeutralButtonLabel() {
+        return null;
     }
 
     @SuppressWarnings({"deprecation","RestrictedApi"})
@@ -115,7 +117,7 @@ public class TimeDialog extends BottomSheetDialogFragment
     }
 
     @Override
-    public void onSaveInstanceState( Bundle outState )
+    public void onSaveInstanceState( @NonNull Bundle outState )
     {
         saveSettings(outState);
         super.onSaveInstanceState(outState);
@@ -123,6 +125,9 @@ public class TimeDialog extends BottomSheetDialogFragment
 
     protected void loadSettings(Bundle bundle) {}
     protected void saveSettings(Bundle bundle) {}
+
+    public void loadSettings(@Nullable Activity activity) {}
+    public void saveSettings(@Nullable Activity activity) {}
 
     private DialogInterface.OnClickListener onAccepted = null;
     public void setOnAcceptedListener( DialogInterface.OnClickListener listener ) {
@@ -151,7 +156,7 @@ public class TimeDialog extends BottomSheetDialogFragment
         @Override
         public void onShow(DialogInterface dialog)
         {
-            ViewUtils.initPeekHeight(dialog, R.id.dialog_footer);
+            ViewUtils.initPeekHeight(dialog, getPeekViewID());
             if (onShowListener != null) {
                 onShowListener.onShow(dialog);
             }
@@ -166,9 +171,12 @@ public class TimeDialog extends BottomSheetDialogFragment
     {
         @Override
         public void onClick(View v) {
-            onDialogAcceptClick.onClick(v);
+            onDialogNeutralClick(v);
         }
     };
+    protected void onDialogNeutralClick(View v) {
+        onDialogAcceptClick.onClick(v);
+    }
 
     protected View.OnClickListener onDialogCancelClick = new View.OnClickListener() {
         @Override
@@ -188,15 +196,22 @@ public class TimeDialog extends BottomSheetDialogFragment
     protected View.OnClickListener onDialogAcceptClick = new View.OnClickListener()
     {
         @Override
-        public void onClick(View v)
-        {
-            dismiss();
-            if (onAccepted != null) {
-                onAccepted.onClick(getDialog(), 0);
-            }
+        public void onClick(View v) {
+            onDialogAccept();
         }
     };
+    protected void onDialogAccept()
+    {
+        saveSettings(getActivity());
+        dismiss();
+        if (onAccepted != null) {
+            onAccepted.onClick(getDialog(), 0);
+        }
+    }
 
+    protected int getPeekViewID() {
+        return R.id.dialog_footer;
+    }
     protected void expandSheet(DialogInterface dialog)
     {
         if (dialog == null) {
@@ -214,45 +229,41 @@ public class TimeDialog extends BottomSheetDialogFragment
         }
     }
 
-    public int getSelectedHour() {
-        return (picker != null ? picker.getHour() : getInitialHour());
-    }
-
-    public int getSelectedMinute() {
-        return (picker != null ? picker.getMinute() : getInitialMinute());
-    }
-
-    public void setInitialTime(String hour, String minute) {
-        try {
-            setInitialTime(Integer.parseInt(hour), Integer.parseInt(minute));
-        } catch (NumberFormatException e) {
-            Log.e("TimeDialog", "setInitialTime: invalid input; " + e);
+    @NonNull
+    public Bundle getArgs()
+    {
+        Bundle args = getArguments();
+        Bundle retValue = args;
+        if (args == null) {
+            setArguments(retValue = new Bundle());
         }
-    }
-    public void setInitialTime(int hour, int minute) {
-        getArguments().putInt(KEY_DIALOG_HOUR, hour);
-        getArguments().putInt(KEY_DIALOG_MINUTE, minute);
-    }
-    public int getInitialHour() {
-        return getArguments().getInt(KEY_DIALOG_HOUR, 12);
-    }
-    public int getInitialMinute() {
-        return getArguments().getInt(KEY_DIALOG_MINUTE, 0);
+        return retValue;
     }
 
     public void setTimeIs24(boolean is24) {
-        getArguments().putBoolean(KEY_DIALOG_IS24, is24);
+        getArgs().putBoolean(KEY_DIALOG_IS24, is24);
     }
     public boolean timeIs24() {
-        return getArguments().getBoolean(KEY_DIALOG_IS24, true);
+        return getArgs().getBoolean(KEY_DIALOG_IS24, true);
     }
 
     @Nullable
     public String getDialogTitle() {
-        return getArguments().getString(KEY_DIALOG_TITLE);
+        return getArgs().getString(KEY_DIALOG_TITLE);
     }
     public void setDialogTitle(@Nullable String title) {
-        getArguments().putString(KEY_DIALOG_TITLE, title);
+        getArgs().putString(KEY_DIALOG_TITLE, title);
+    }
+
+    public abstract TimeDialogResult getSelected();
+    public interface TimeDialogResult
+    {
+        Integer getYear();
+        Integer getMonth();
+        Integer getDay();
+        Integer getHour();
+        Integer getMinute();
+        Integer getSecond();
     }
 
 }
