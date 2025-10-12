dbc7ab3ce229a5f4e84b7fafa3ae3578662b5c16
Forrest Guice <forrestguice@users.noreply.github.com>
2025-10-11 20:16:00 -0700

    BaseEvent
    refactor

--- app/src/main/java/com/forrestguice/suntimeswidget/events/ElevationEvent.java
+++ app/src/main/java/com/forrestguice/suntimeswidget/events/ElevationEvent.java
@@ -1,20 +1,35 @@
+/**
+    Copyright (C) 2023-2025 Forrest Guice
+    This file is part of SuntimesWidget.
+
+    SuntimesWidget is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SuntimesWidget is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SuntimesWidget.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.forrestguice.suntimeswidget.events;
 
 import com.forrestguice.suntimeswidget.R;
-import com.forrestguice.suntimeswidget.calculator.settings.SuntimesDataSettings;
 import com.forrestguice.suntimeswidget.calculator.settings.display.TimeDeltaDisplay;
 import com.forrestguice.util.Resources;
 
-import static com.forrestguice.suntimeswidget.alarmclock.AlarmEventContract.AUTHORITY;
-
-public abstract class ElevationEvent
+public abstract class ElevationEvent extends BaseEvent
 {
     public static final String SUFFIX_RISING = "r";
     public static final String SUFFIX_SETTING = "s";
 
     public ElevationEvent(double angle, int offset, boolean rising) {
+        super(offset);
         this.angle = angle;
-        this.offset = offset;
         this.rising = rising;
     }
 
@@ -26,21 +41,12 @@ public abstract class ElevationEvent
         angle = value;
     }
 
-    protected int offset;            // milliseconds
-    public int getOffset() {
-        return offset;
-    }
-
     protected boolean rising;
     public boolean isRising() {
         return rising;
     }
 
-    protected String getUri() {
-        return EventUri.getEventCalcUri(AUTHORITY, getEventName());
-    }
-
-    private static final TimeDeltaDisplay utils = new TimeDeltaDisplay();
+    @Override
     public String offsetDisplay(Resources context)
     {
         if (offset != 0)
@@ -50,10 +56,4 @@ public abstract class ElevationEvent
             return context.getQuantityString((offset < 0 ? R.plurals.offset_before_plural : R.plurals.offset_after_plural), (int)angle, offsetDisplay);
         } else return "";
     }
-
-    public abstract String getEventName();
-    public abstract String getEventTitle(SuntimesDataSettings settings);
-    public abstract String getEventPhrase(SuntimesDataSettings settings);
-    public abstract String getEventGender(SuntimesDataSettings settings);
-    public abstract String getEventSummary(SuntimesDataSettings settings);
 }
