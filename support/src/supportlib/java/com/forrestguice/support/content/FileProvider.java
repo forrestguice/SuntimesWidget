f7124416ac16a749c9a84bf94add4d7b485c67b6
Forrest Guice <forrestguice@users.noreply.github.com>
2026-01-17 12:27:54 -0700

    FileProvider
    fix imports

--- support/src/supportlib/java/com/forrestguice/support/content/FileProvider.java
+++ support/src/supportlib/java/com/forrestguice/support/content/FileProvider.java
@@ -1,5 +1,10 @@
 package com.forrestguice.support.content;
 
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+
 public class FileProvider extends android.support.v4.content.FileProvider {
 
     public static File getExternalStorageDownloadDirectory(Context context) {
