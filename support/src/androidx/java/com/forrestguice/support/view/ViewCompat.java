3893cd5f368b08b8b83d44ba4b446d26e7e281f7
Forrest Guice <forrestguice@users.noreply.github.com>
2026-01-16 18:27:55 -0700

    ViewCompat
    un-deprecate `getTranslationX` and `getTranslationY`

--- support/src/androidx/java/com/forrestguice/support/view/ViewCompat.java
+++ support/src/androidx/java/com/forrestguice/support/view/ViewCompat.java
@@ -2,6 +2,8 @@ package com.forrestguice.support.view;
 
 import android.content.res.ColorStateList;
 import androidx.core.widget.CompoundButtonCompat;
+
+import android.view.View;
 import android.widget.CompoundButton;
 
 import com.forrestguice.annotation.NonNull;
@@ -11,5 +13,15 @@ public class ViewCompat extends androidx.core.view.ViewCompat
 {
     public static void setButtonTintList(@NonNull CompoundButton button, @Nullable ColorStateList tint) {
         CompoundButtonCompat.setButtonTintList(button, tint);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static float getTranslationX(View view) {
+        return view.getTranslationX();
+    }
+
+    @SuppressWarnings("deprecation")
+    public static float getTranslationY(View view) {
+        return view.getTranslationY();
     }
 }
